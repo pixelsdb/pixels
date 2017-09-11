37fa245186b92facca5f90dfc7fbed180120ea85
Guodong Jin <jelly.guodong.jin@gmail.com>
2017-09-11 15:21:08 +0000

    Add constants to TestParams and change `read` to `readFully`

--- core/src/test/java/cn/edu/ruc/iir/pixels/core/reader/TestParams.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.ruc.iir.pixels.core.reader;
-
-public class TestParams
-{
-    public static final String filePath = "hdfs://127.0.0.1:9000/test3_string.pxl";
-    public static final String metaPath = System.getProperty("user.home") + "/meta";
-}
