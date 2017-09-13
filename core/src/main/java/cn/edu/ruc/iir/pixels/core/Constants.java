283721ef45e4bd57d1347c2b04bd9278128e126a
Haoqiong Bian <bianhaoqiong@gmail.com>
2017-09-13 13:32:13 +0000

    add pixels compactor

--- core/src/main/java/cn/edu/ruc/iir/pixels/core/Constants.java
+++ core/src/main/java/cn/edu/ruc/iir/pixels/core/Constants.java
@@ -7,8 +7,8 @@ package cn.edu.ruc.iir.pixels.core;
  */
 public final class Constants
 {
-    static int VERSION = 1;
+    public static int VERSION = 1;
     //static int MB1 = 1024 * 1024;
-    static int HDFS_BUFFER_SIZE = 256 * 1024;
-    static String MAGIC = "PIXELS";
+    public static int HDFS_BUFFER_SIZE = 256 * 1024;
+    public static String MAGIC = "PIXELS";
 }
