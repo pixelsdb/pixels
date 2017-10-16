eaba86ad32269edfa115b0e44424453dded1bbea
Guodong Jin <jelly.guodong.jin@gmail.com>
2017-10-16 13:59:32 +0000

    add run length encoding

--- core/src/main/java/cn/edu/ruc/iir/pixels/core/Constants.java
+++ core/src/main/java/cn/edu/ruc/iir/pixels/core/Constants.java
@@ -11,4 +11,7 @@ public final class Constants
     //static int MB1 = 1024 * 1024;
     public static int HDFS_BUFFER_SIZE = 256 * 1024;
     public static String MAGIC = "PIXELS";
+    public static int MIN_REPEAT= 3;
+    public static int MAX_SCOPE = 512;
+    public static int MAX_SHORT_REPEAT_LENGTH = 10;
 }
---
+++ core/src/main/java/cn/edu/ruc/iir/pixels/core/encoding/Decoder.java
@@ -0,0 +1,13 @@
+package cn.edu.ruc.iir.pixels.core.encoding;
+
+import java.io.IOException;
+
+/**
+ * pixels
+ *
+ * @author guodong
+ */
+public abstract class Decoder
+{
+    public abstract boolean hasNext() throws IOException;
+}
