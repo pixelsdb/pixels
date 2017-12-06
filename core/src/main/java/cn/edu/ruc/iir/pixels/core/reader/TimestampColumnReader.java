79076c6edf6e22fd52278d92c7eb40967c29efd9
Guodong Jin <jelly.guodong.jin@gmail.com>
2017-12-06 13:57:42 +0000

    pixels reader without predicate filterings temp sync

---
+++ core/src/main/java/cn/edu/ruc/iir/pixels/core/reader/TimestampColumnReader.java
@@ -0,0 +1,13 @@
+package cn.edu.ruc.iir.pixels.core.reader;
+
+/**
+ * pixels
+ *
+ * @author guodong
+ */
+public class TimestampColumnReader extends BaseColumnReader
+{
+    public TimestampColumnReader()
+    {
+    }
+}
