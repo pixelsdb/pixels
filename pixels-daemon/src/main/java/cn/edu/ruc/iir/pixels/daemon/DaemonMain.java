caf9553d1854f7e0bc7dd06d110bf00fe4e60fcb
Haoqiong Bian <bianhaoqiong@gmail.com>
2018-01-10 03:37:09 +0800

    impl guard daemon

--- pixels-daemon/src/main/java/cn/edu/ruc/iir/pixels/daemon/DaemonMain.java
+++ pixels-daemon/src/main/java/cn/edu/ruc/iir/pixels/daemon/DaemonMain.java
@@ -2,6 +2,9 @@ package cn.edu.ruc.iir.pixels.daemon;
 
 import cn.edu.ruc.iir.pixels.common.ConfigFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 public class DaemonMain
@@ -14,6 +17,39 @@ public class DaemonMain
             if (args[0].equalsIgnoreCase("main"))
             {
                 isMain = true;
+            } else if (args[0].equalsIgnoreCase("shutdown"))
+            {
+                System.out.println("Shutdown Daemons..");
+                try
+                {
+                    for (int i = 0; i < 3; ++i)
+                    {
+                        Process process = Runtime.getRuntime().exec("jps -l");
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        String line;
+                        while ((line = reader.readLine()) != null)
+                        {
+                            System.out.println(line);
+                            if (line.toLowerCase().contains("pixels-daemon"))
+                            {
+                                String[] strings = line.split("\\s{1,}");
+                                int pid = Integer.parseInt(strings[0]);
+                                System.out.println(pid);
+                                Runtime.getRuntime().exec("kill -9 " + pid);
+                            }
+                        }
+                        process.waitFor();
+                        reader.close();
+                        process.destroy();
+                    }
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                System.exit(0);
             }
         }
 
@@ -42,8 +78,7 @@ public class DaemonMain
                     e.printStackTrace();
                 }
             }
-        }
-        else
+        } else
         {
             // this is the guard daemon
             GuardDaemon guardDaemon = new GuardDaemon();
