be066f157cbc71b2e8cf7ac09b12a38105145153
Guodong Jin <jelly.guodong.jin@gmail.com>
2018-06-17 21:04:03 +0800

    add optimization for double/float/int/timestamp; string is a little bit complex, it's left to be done later

--- pixels-core/src/test/java/cn/edu/ruc/iir/pixels/core/TestByteBuf.java
+++ pixels-core/src/test/java/cn/edu/ruc/iir/pixels/core/TestByteBuf.java
@@ -3,6 +3,7 @@ package cn.edu.ruc.iir.pixels.core;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.buffer.Unpooled;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
@@ -53,11 +54,32 @@ public class TestByteBuf
     @Test
     public void test()
     {
-        byte[][] matrix = new byte[10][10];
-        byte[] row = matrix[0];
-        System.out.println(row[0]);
-        matrix[0] = null;
-        System.out.println(row[0]);
+        byte[] input = new byte[100000];
+        long bufferStart = System.currentTimeMillis();
+        ByteBuffer buffer = ByteBuffer.wrap(input);
+        long bufferEnd = System.currentTimeMillis();
+        System.out.println("ByteBuffer wrap cost: " + (bufferEnd - bufferStart));
+        int sum = 0;
+        long bufferReadStart = System.nanoTime();
+        for (int i = 0; i < 10000; i++)
+        {
+            sum += buffer.getInt();
+        }
+        long bufferReadEnd = System.nanoTime();
+        System.out.println("ByteBuffer read cost: " + (bufferReadEnd - bufferReadStart) + ", sum: " + sum);
+
+        long bufStart = System.currentTimeMillis();
+        ByteBuf buf = Unpooled.wrappedBuffer(input);
+        long bufEnd = System.currentTimeMillis();
+        System.out.println("ByteBuf wrap cost: " + (bufEnd - bufStart));
+        sum = 0;
+        long bufReadStart = System.nanoTime();
+        for (int i = 0; i < 10000; i++)
+        {
+            sum += buf.readInt();
+        }
+        long bufReadEnd = System.nanoTime();
+        System.out.println("ByteBuf read cost: " + (bufReadEnd - bufReadStart) + ", sum: " + sum);
     }
 
     @Test
