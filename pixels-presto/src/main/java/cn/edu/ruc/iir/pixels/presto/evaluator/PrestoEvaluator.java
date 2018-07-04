acc17398d4caac34105c92797992e077f759ffe8
taoyouxian <taoyouxian@aliyun.com>
2018-07-04 15:18:32 +0800

    add test

--- pixels-presto/src/main/java/cn/edu/ruc/iir/pixels/presto/evaluator/PrestoEvaluator.java
+++ pixels-presto/src/main/java/cn/edu/ruc/iir/pixels/presto/evaluator/PrestoEvaluator.java
@@ -21,26 +21,46 @@ public class PrestoEvaluator {
     public static long execute(String jdbcUrl, Properties jdbcProperties, String tableName, String columns, String orderByColumn) {
         String sql = "";
         long start = 0L, end = 0L;
-        try
-        {
+        try {
             Thread.sleep(1000);
-        } catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             e.printStackTrace();
             throw new PrestoException(PIXELS_THREAD_ERROR, e);
         }
         try (Connection connection = DriverManager.getConnection(jdbcUrl, jdbcProperties)) {
             Statement statement = connection.createStatement();
             sql = "select " + columns + " from " + tableName + " order by " + orderByColumn + " limit 10";
-             start = System.currentTimeMillis();
+            start = System.currentTimeMillis();
             ResultSet resultSet = statement.executeQuery(sql);
             resultSet.next();
             end = System.currentTimeMillis();
             statement.close();
         } catch (SQLException e) {
-            e.printStackTrace();
             System.out.println("SQL: " + sql);
+            System.out.println("Error msg: " + e.getMessage());
             throw new PrestoException(PIXELS_SQL_EXECUTE_ERROR, e);
+        }
+        return end - start;
+    }
+
+    public static long executeSQL(String jdbcUrl, Properties jdbcProperties, String sql, String id) {
+        long start = 0L, end = 0L;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new PrestoException(PIXELS_THREAD_ERROR, e);
+        }
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, jdbcProperties)) {
+            Statement statement = connection.createStatement();
+            start = System.currentTimeMillis();
+            ResultSet resultSet = statement.executeQuery(sql);
+            resultSet.next();
+            end = System.currentTimeMillis();
+            statement.close();
+        } catch (SQLException e) {
+            System.out.println("SQL: " + id + "\n" + sql);
+            System.out.println("Error msg: " + e.getMessage());
         }
         return end - start;
     }
