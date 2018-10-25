835afc34284d1fc01b65ededf859ed6a4dff8afb
taoyouxian <taoyouxian@aliyun.com>
2018-10-25 22:01:33 +0800

    add simple lock

--- pixels-common/src/main/java/cn/edu/ruc/iir/pixels/common/utils/EtcdUtil.java
+++ pixels-common/src/main/java/cn/edu/ruc/iir/pixels/common/utils/EtcdUtil.java
@@ -4,6 +4,7 @@ import com.coreos.jetcd.Client;
 import com.coreos.jetcd.Watch;
 import com.coreos.jetcd.data.ByteSequence;
 import com.coreos.jetcd.data.KeyValue;
+import com.coreos.jetcd.kv.DeleteResponse;
 import com.coreos.jetcd.kv.PutResponse;
 import com.coreos.jetcd.lease.LeaseGrantResponse;
 import com.coreos.jetcd.options.DeleteOption;
@@ -23,28 +24,25 @@ import java.util.concurrent.ExecutionException;
  * Created at: 18-10-14
  * Author: hank
  */
-public class EtcdUtil
-{
+public class EtcdUtil {
     private static EtcdUtil instance = new EtcdUtil();
     private static Logger logger = LoggerFactory.getLogger(EtcdUtil.class);
     private Client client = null;
+    private boolean lockHeld;
 
-    private EtcdUtil()
-    {
+    private EtcdUtil() {
         String[] hosts = ConfigFactory.Instance().getProperty("etcd.hosts").split(",");
         Random random = new Random(System.nanoTime());
         String host = hosts[random.nextInt(hosts.length)];
         System.out.println(host);
         String port = ConfigFactory.Instance().getProperty("etcd.port");
 
-        if (this.client == null)
-        {
+        if (this.client == null) {
             this.client = Client.builder().endpoints("http://" + host + ":" + port).build();
         }
     }
 
-    public static EtcdUtil Instance ()
-    {
+    public static EtcdUtil Instance() {
         return instance;
     }
 
@@ -53,8 +51,7 @@ public class EtcdUtil
      *
      * @return
      */
-    public Client getClient()
-    {
+    public Client getClient() {
         return client;
     }
 
@@ -65,18 +62,14 @@ public class EtcdUtil
      * @param key etcdKey
      * @return
      */
-    public KeyValue getKeyValue(String key)
-    {
+    public KeyValue getKeyValue(String key) {
         KeyValue keyValue = null;
-        try
-        {
+        try {
             List<KeyValue> keyValues = this.client.getKVClient().get(ByteSequence.fromString(key)).get().getKvs();
-            if (keyValues.size() > 0)
-            {
+            if (keyValues.size() > 0) {
                 keyValue = keyValues.get(0);
             }
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("error when get key-value by key.", e);
         }
         return keyValue;
@@ -88,15 +81,12 @@ public class EtcdUtil
      * @param prefix the prefix
      * @return
      */
-    public List<KeyValue> getKeyValuesByPrefix(String prefix)
-    {
+    public List<KeyValue> getKeyValuesByPrefix(String prefix) {
         List<KeyValue> keyValues = new ArrayList<>();
         GetOption getOption = GetOption.newBuilder().withPrefix(ByteSequence.fromString(prefix)).build();
-        try
-        {
+        try {
             keyValues = this.client.getKVClient().get(ByteSequence.fromString(prefix), getOption).get().getKvs();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("error when get key-values by prefix.", e);
         }
         return keyValues;
@@ -108,14 +98,11 @@ public class EtcdUtil
      * @param key
      * @param value
      */
-    public void putKeyValue(String key, String value)
-    {
+    public void putKeyValue(String key, String value) {
         CompletableFuture<PutResponse> future = client.getKVClient().put(ByteSequence.fromString(key), ByteSequence.fromString(value));
-        try
-        {
+        try {
             future.get();
-        } catch (InterruptedException | ExecutionException e)
-        {
+        } catch (InterruptedException | ExecutionException e) {
             logger.error("error when put key-value into etcd.", e);
         }
     }
@@ -128,17 +115,14 @@ public class EtcdUtil
      * @param expireTime expire time in seconds.
      * @return lease id, 0L if error occurs.
      */
-    public long putKeyValueWithExpireTime(String key, String value, long expireTime)
-    {
+    public long putKeyValueWithExpireTime(String key, String value, long expireTime) {
         CompletableFuture<LeaseGrantResponse> leaseGrantResponse = this.client.getLeaseClient().grant(expireTime);
         PutOption putOption;
-        try
-        {
+        try {
             putOption = PutOption.newBuilder().withLeaseId(leaseGrantResponse.get().getID()).build();
             this.client.getKVClient().put(ByteSequence.fromString(key), ByteSequence.fromString(value), putOption);
             return leaseGrantResponse.get().getID();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("error when put key-value with expire time into etcd.", e);
         }
         return 0L;
@@ -152,15 +136,12 @@ public class EtcdUtil
      * @param leaseId lease id
      * @return revision id, 0L if error occurs.
      */
-    public long putKeyValueWithLeaseId(String key, String value, long leaseId) throws Exception
-    {
+    public long putKeyValueWithLeaseId(String key, String value, long leaseId) throws Exception {
         PutOption putOption = PutOption.newBuilder().withLeaseId(leaseId).build();
         CompletableFuture<PutResponse> putResponse = this.client.getKVClient().put(ByteSequence.fromString(key), ByteSequence.fromString(value), putOption);
-        try
-        {
+        try {
             return putResponse.get().getHeader().getRevision();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.error("error when put key-value with lease id into etcd.", e);
         }
         return 0L;
@@ -171,8 +152,7 @@ public class EtcdUtil
      *
      * @param leaseId
      */
-    public void keepLeaseAlive(long leaseId)
-    {
+    public void keepLeaseAlive(long leaseId) {
         this.client.getLeaseClient().keepAlive(leaseId);
     }
 
@@ -181,8 +161,7 @@ public class EtcdUtil
      *
      * @param key
      */
-    public void delete(String key)
-    {
+    public void delete(String key) {
         this.client.getKVClient().delete(ByteSequence.fromString(key));
     }
 
@@ -191,8 +170,7 @@ public class EtcdUtil
      *
      * @param prefix
      */
-    public void deleteByPrefix(String prefix)
-    {
+    public void deleteByPrefix(String prefix) {
         DeleteOption deleteOption = DeleteOption.newBuilder().withPrefix(ByteSequence.fromString(prefix)).build();
         this.client.getKVClient().delete(ByteSequence.fromString(prefix), deleteOption);
     }
@@ -203,8 +181,7 @@ public class EtcdUtil
      * @param key
      * @return
      */
-    public Watch.Watcher getCustomWatcherForKey(String key)
-    {
+    public Watch.Watcher getCustomWatcherForKey(String key) {
         return this.client.getWatchClient().watch(ByteSequence.fromString(key));
     }
 
@@ -214,10 +191,81 @@ public class EtcdUtil
      * @param prefix
      * @return
      */
-    public Watch.Watcher getCustomWatcherForPrefix(String prefix)
-    {
+    public Watch.Watcher getCustomWatcherForPrefix(String prefix) {
         WatchOption watchOption = WatchOption.newBuilder().withPrefix(ByteSequence.fromString(prefix)).build();
         return this.client.getWatchClient().watch(ByteSequence.fromString(prefix), watchOption);
     }
 
+    private String name;
+
+    /**
+     * Attempt to acquire a lock.
+     *
+     * @return boolean indicating success
+     */
+    synchronized public boolean acquire(String name, String lockContent) {
+        this.name = name;
+        CompletableFuture<PutResponse> future = client.getKVClient().put(ByteSequence.fromString(name), ByteSequence.fromString(lockContent));
+        PutResponse response = null;
+        try {
+            response = future.get();
+            lockHeld = true;
+        } catch (ExecutionException | InterruptedException e) {
+            logger.error("Error encountered when attempting to acquire lock", e);
+            lockHeld = false;
+        }
+        return lockHeld;
+    }
+
+    synchronized public boolean renew(String name, String lockContent) throws Exception {
+        // validate that we have a lock
+        if (!lockHeld) {
+            throw new Exception("Lock cannot be released unless first acquired");
+        }
+
+        CompletableFuture<PutResponse> future = client.getKVClient().put(ByteSequence.fromString(name), ByteSequence.fromString(lockContent));
+        PutResponse response = null;
+        try {
+            response = future.get();
+        } catch (ExecutionException | InterruptedException e) {
+            logger.error("Error encountered when attempting to acquire lock", e);
+        }
+        return true;
+    }
+
+    /**
+     * Release the currently held lock.
+     *
+     * @return boolean indicating success
+     * @throws Exception If no lock is held
+     */
+    synchronized public boolean release() throws Exception {
+        // validate that we have a lock
+        if (!lockHeld) {
+            throw new Exception("Lock cannot be released unless first acquired");
+        }
+        // attempt to release the lock
+        DeleteResponse response;
+        try {
+            response = client.getKVClient().delete(ByteSequence.fromString(name)).get();
+            lockHeld = false;
+            return true;
+        } catch (Exception e) {
+            logger.error("Lock could not be released", e);
+        }
+        return false;
+    }
+
+    public void close() {
+        // attempt to close the connection
+        if (lockHeld) {
+            try {
+                release();
+            } catch (Exception e) {
+                logger.error("Lock could not be released", e);
+            }
+        }
+        // close the Etcd connection
+        client.close();
+    }
 }
