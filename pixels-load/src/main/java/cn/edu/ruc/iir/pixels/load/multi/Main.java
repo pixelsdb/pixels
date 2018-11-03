7b32a283206da148368c895ca72e72a2487ac5d6
Haoqiong Bian <bianhaoqiong@gmail.com>
2018-11-04 02:21:24 +0800

    remove ddl from pixels-load

--- pixels-load/src/main/java/cn/edu/ruc/iir/pixels/load/multi/Main.java
+++ pixels-load/src/main/java/cn/edu/ruc/iir/pixels/load/multi/Main.java
@@ -2,15 +2,12 @@ package cn.edu.ruc.iir.pixels.load.multi;
 
 import cn.edu.ruc.iir.pixels.common.physical.FSFactory;
 import cn.edu.ruc.iir.pixels.common.utils.ConfigFactory;
-import cn.edu.ruc.iir.pixels.load.util.LoaderUtil;
-import com.facebook.presto.sql.parser.SqlParser;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 import org.apache.hadoop.fs.Path;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
@@ -27,10 +24,6 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * pixels loader command line tool
- * <p>
- * DDL -s /home/tao/software/data/pixels/DDL.txt -d pixels
- * DDL -s /home/tao/software/data/pixels/test30G_pixels/105/presto_ddl.sql -d pixels
- * <p>
  * <p>
  * LOAD -f pixels -o hdfs://dbiir01:9000/pixels/pixels/test_105/source -d pixels -t test_105 -n 300000 -r \t -c 10
  * -p false [optional, default false]
@@ -70,50 +63,12 @@ public class Main
             if (inputStr.equalsIgnoreCase("help") || inputStr.equalsIgnoreCase("-h"))
             {
                 System.out.println("Supported commands:\n" +
-                        "DDL\n" +
                         "LOAD\n");
                 System.out.println("{command} -h to show the usage of a command.\nexit / quit / -q to exit.\n");
                 continue;
             }
 
             String command = inputStr.trim().split("\\s+")[0].toUpperCase();
-
-            if (command.equals("DDL"))
-            {
-                ArgumentParser argumentParser = ArgumentParsers.newArgumentParser("Pixels ETL DDL")
-                        .defaultHelp(true);
-                argumentParser.addArgument("-s", "--schema_file").required(true)
-                        .help("specify the path of schema file");
-                argumentParser.addArgument("-d", "--db_name").required(true)
-                        .help("specify the name of database");
-                Namespace namespace;
-                try
-                {
-                    namespace = argumentParser.parseArgs(inputStr.substring(command.length()).trim().split("\\s+"));
-                } catch (ArgumentParserException e)
-                {
-                    argumentParser.handleError(e);
-                    continue;
-                }
-
-                SqlParser parser = new SqlParser();
-                String dbName = namespace.getString("db_name");
-                String schemaPath = namespace.getString("schema_file");
-
-                try
-                {
-                    if (LoaderUtil.executeDDL(parser, dbName, schemaPath))
-                    {
-                        System.out.println("Executing command " + command + " successfully");
-                    } else
-                    {
-                        System.out.println("Executing command " + command + " unsuccessfully when adding table info");
-                    }
-                } catch (SQLException e)
-                {
-                    e.printStackTrace();
-                }
-            }
 
             if (command.equals("LOAD"))
             {
@@ -204,7 +159,7 @@ public class Main
                 }
             }
 
-            if (!command.equals("DDL") && !command.equals("LOAD"))
+            if (!command.equals("LOAD"))
             {
                 System.out.println("Command error");
             }
