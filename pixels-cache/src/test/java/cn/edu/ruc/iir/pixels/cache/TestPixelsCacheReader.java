a4a71c29da8e5f71286f0f11dedd2edabe9f18e3
Guodong Jin <guod.jin@gmail.com>
2018-11-23 00:49:26 +0800

    change reader test

--- pixels-cache/src/test/java/cn/edu/ruc/iir/pixels/cache/TestPixelsCacheReader.java
+++ pixels-cache/src/test/java/cn/edu/ruc/iir/pixels/cache/TestPixelsCacheReader.java
@@ -2,6 +2,8 @@ package cn.edu.ruc.iir.pixels.cache;
 
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
+
 /**
  * pixels
  *
@@ -19,6 +21,16 @@ public class TestPixelsCacheReader
                     .setIndexLocation("/Users/Jelly/Desktop/pixels.index")
                     .setIndexSize(1024*1024*64L)
                     .build();
+            String path = "/pixels/pixels/test_105/2121211211212.pxl";
+            int index = 0;
+            for (short i = 0; i < 1000; i++) {
+                for (short j = 0; j < 64; j++) {
+                    byte[] value = cacheReader.get(path, i, j);
+                    ByteBuffer buffer = ByteBuffer.wrap(value);
+                    assert buffer.getInt() == index;
+                    index++;
+                }
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
