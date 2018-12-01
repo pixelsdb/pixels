f4c79c4366a0b73cc725c62580e0c6d2465a08dd
Haoqiong Bian <bianhaoqiong@gmail.com>
2018-12-02 04:56:40 +0800

    update balancer and metadata

--- pixels-load/src/main/java/cn/edu/ruc/iir/pixels/load/multi/Main.java
+++ pixels-load/src/main/java/cn/edu/ruc/iir/pixels/load/multi/Main.java
@@ -25,7 +25,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 /**
  * pixels loader command line tool
  * <p>
- * LOAD -f pixels -o hdfs://dbiir10:9000/pixels/pixels/test_105/source -d pixels -t test_105_perf -n 300000 -r \t -c 16
+ * LOAD -f pixels -o hdfs://dbiir10:9000/pixels/pixels/test_1187/source -d pixels -t test_1187 -n 25000 -r \t -c 16
  * -p false [optional, default false]
  * </p>
  */
---
+++ pixels-presto/src/main/java/cn/edu/ruc/iir/pixels/presto/exception/BalancerException.java
@@ -0,0 +1,13 @@
+package cn.edu.ruc.iir.pixels.presto.exception;
+
+/**
+ * Created at: 18-12-2
+ * Author: hank
+ */
+public class BalancerException extends Exception
+{
+    public BalancerException (String msg)
+    {
+        super(msg);
+    }
+}
