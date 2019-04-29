e482f7927158dd57b8ad0e3a8118a107381ee9ea
Guodong Jin <guod.jin@gmail.com>
2019-04-29 17:05:40 +0800

    add test classes: merge from branch raf

---
+++ pixels-tools/src/main/java/cn/edu/ruc/iir/pixels/test/MemMappedFilePerf.java
@@ -0,0 +1,19 @@
+package cn.edu.ruc.iir.pixels.test;
+
+import cn.edu.ruc.iir.pixels.cache.MemoryMappedFile;
+
+public class MemMappedFilePerf
+{
+    public static void main(String[] args)
+    {}
+
+    static class MemMappedReader implements Runnable
+    {
+        private MemoryMappedFile file;
+
+        @Override
+        public void run()
+        {
+        }
+    }
+}
