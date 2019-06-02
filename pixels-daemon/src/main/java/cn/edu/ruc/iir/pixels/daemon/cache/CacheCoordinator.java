95027ab4f0e9d319968327bb757d8a74ad8213f1
guodong <guod.jin@gmail.com>
2019-06-02 20:31:14 +0800

    add todo for cache balancing

--- pixels-daemon/src/main/java/cn/edu/ruc/iir/pixels/daemon/cache/CacheCoordinator.java
+++ pixels-daemon/src/main/java/cn/edu/ruc/iir/pixels/daemon/cache/CacheCoordinator.java
@@ -16,6 +16,7 @@ import com.coreos.jetcd.options.WatchOption;
 import com.coreos.jetcd.watch.WatchEvent;
 import com.coreos.jetcd.watch.WatchResponse;
 import com.facebook.presto.spi.HostAddress;
+import com.facebook.presto.spi.PrestoException;
 import com.google.common.collect.ImmutableList;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.BlockLocation;
@@ -272,6 +273,7 @@ public class CacheCoordinator
             nodesCacheStats.put(nodes[i].toString(), 0);
         }
 
+        // locality-based balancing
         for (String path : paths)
         {
             // get a set of nodes where the blocks of the file is located (location_set)
@@ -302,6 +304,8 @@ public class CacheCoordinator
                 locationDistribution.addCacheLocation(chosenLocation.toString(), path);
             }
         }
+
+        // TODO: if remote caching is enabled, balance cache locations further.
 
         return locationDistribution;
     }
