e3a429631d419b7dda4c6e4cc5f169ce401667c1
Haoqiong Bian (Hank) <bianhaoqiong@gmail.com>
2020-09-13 21:24:01 +0800

    HOTFIX: delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-node="dbiir27"
-
-mvn clean package
-
-ssh -tt iir@$node /home/iir/opt/presto-server/sbin/stop-all.sh
-scp -r ./pixels-presto/target/pixels-presto-0.1.0-SNAPSHOT iir@$node:~/opt/presto-server/plugin/
-ssh -tt iir@$node /home/iir/opt/presto-server/sbin/mv-plugins.sh
