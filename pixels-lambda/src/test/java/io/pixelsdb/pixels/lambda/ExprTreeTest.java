a4320c9535c5cd10f3e607cbd40cbb0982e5a529
Haoqiong Bian <bianhaoqiong@gmail.com>
2022-03-30 18:21:18 +0200

    [Issue #170]: clean the unused files and reformat the code. (#247)

--- pixels-lambda/src/test/java/io/pixelsdb/pixels/lambda/ExprTreeTest.java
+++ pixels-lambda/src/test/java/io/pixelsdb/pixels/lambda/ExprTreeTest.java
@@ -9,12 +9,13 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 
-public class ExprTreeTest {
+public class ExprTreeTest
+{
     ExprTree bigintFilter = new ExprTree("aaa", ExprTree.Operator.EQ, "3");
     TypeDescription schema = TypeDescription.fromString("struct<aaa:bigint, bbb:bigint, " +
             "ccc:varchar(79), ddd:date, eee:decimal(15,2), fff:char(1)>");
     VectorizedRowBatch oldRowBatch = schema.createRowBatch();
-    ExprTree bigintFilter2 = new ExprTree("bbb", ExprTree.Operator.GT,"2");
+    ExprTree bigintFilter2 = new ExprTree("bbb", ExprTree.Operator.GT, "2");
     ExprTree exprTree3 = new ExprTree(ExprTree.Operator.AND, bigintFilter, bigintFilter2);
     ExprTree exprTree4 = new ExprTree(ExprTree.Operator.OR, bigintFilter, bigintFilter2);
     ExprTree emptyFilter = new ExprTree();
@@ -28,7 +29,8 @@ public class ExprTreeTest {
     ExprTree twoLevelFilter = new ExprTree(ExprTree.Operator.OR, orderStatusFilter, bigintFilter);
 
 
-    ArrayList<String> filesToScan = new ArrayList<String>() {
+    ArrayList<String> filesToScan = new ArrayList<String>()
+    {
         {
             add("pixels-tpch-orders-v-0-order/20220312072707_0.pxl");
             add("pixels-tpch-orders-v-0-order/20220312072714_1.pxl");
@@ -37,7 +39,8 @@ public class ExprTreeTest {
         }
     };
 
-    ArrayList<String> cols = new ArrayList<String>() {
+    ArrayList<String> cols = new ArrayList<String>()
+    {
         {
             add("o_orderkey");
             add("o_custkey");
@@ -47,40 +50,58 @@ public class ExprTreeTest {
     };
 
     @Before
-    public void initializeOldRowBatch() {
+    public void initializeOldRowBatch()
+    {
         bigintFilter.prepare(schema);
         bigintFilter2.prepare(schema);
         exprTree3.prepare(schema);
         exprTree4.prepare(schema);
         oldRowBatch.size = 5;
-        oldRowBatch.cols[0].add(2);oldRowBatch.cols[1].add(5);oldRowBatch.cols[2].add("kobe");
-        oldRowBatch.cols[0].add(3);oldRowBatch.cols[1].add(2);oldRowBatch.cols[2].add("bryant");
-        oldRowBatch.cols[0].add(4);oldRowBatch.cols[1].add(2);oldRowBatch.cols[2].add("kobe");
-        oldRowBatch.cols[0].add(5);oldRowBatch.cols[1].add(2);oldRowBatch.cols[2].add("bryant");
-        oldRowBatch.cols[0].add(3);oldRowBatch.cols[1].add(4);oldRowBatch.cols[2].add("antisocial");
+        oldRowBatch.cols[0].add(2);
+        oldRowBatch.cols[1].add(5);
+        oldRowBatch.cols[2].add("kobe");
+        oldRowBatch.cols[0].add(3);
+        oldRowBatch.cols[1].add(2);
+        oldRowBatch.cols[2].add("bryant");
+        oldRowBatch.cols[0].add(4);
+        oldRowBatch.cols[1].add(2);
+        oldRowBatch.cols[2].add("kobe");
+        oldRowBatch.cols[0].add(5);
+        oldRowBatch.cols[1].add(2);
+        oldRowBatch.cols[2].add("bryant");
+        oldRowBatch.cols[0].add(3);
+        oldRowBatch.cols[1].add(4);
+        oldRowBatch.cols[2].add("antisocial");
         oldRowBatch.cols[3].add("1981-01-01"); // these might be off by one day due to time zone shit
         oldRowBatch.cols[3].add("1981-02-01"); // but should be fine because we are comparing two days
         oldRowBatch.cols[3].add("1981-03-01");
         oldRowBatch.cols[3].add("1981-05-01");
         oldRowBatch.cols[3].add("1981-12-01");
-        oldRowBatch.cols[4].add(3.23);oldRowBatch.cols[5].add("P");
-        oldRowBatch.cols[4].add(5.43);oldRowBatch.cols[5].add("F");
-        oldRowBatch.cols[4].add(2.13);oldRowBatch.cols[5].add("P");
-        oldRowBatch.cols[4].add(6.00);oldRowBatch.cols[5].add("F");
-        oldRowBatch.cols[4].add(5.02);oldRowBatch.cols[5].add("O");
+        oldRowBatch.cols[4].add(3.23);
+        oldRowBatch.cols[5].add("P");
+        oldRowBatch.cols[4].add(5.43);
+        oldRowBatch.cols[5].add("F");
+        oldRowBatch.cols[4].add(2.13);
+        oldRowBatch.cols[5].add("P");
+        oldRowBatch.cols[4].add(6.00);
+        oldRowBatch.cols[5].add("F");
+        oldRowBatch.cols[4].add(5.02);
+        oldRowBatch.cols[5].add("O");
 
         System.out.println("oldRowBatch:");
         System.out.println(oldRowBatch);
     }
 
     @Test
-    public void testEvaluateWorks() {
+    public void testEvaluateWorks()
+    {
         boolean evalRes = bigintFilter.evaluate(oldRowBatch, 1);
         System.out.println(evalRes);
     }
 
     @Test
-    public void testBigIntFilterRowBatch() {
+    public void testBigIntFilterRowBatch()
+    {
 
         VectorizedRowBatch newRowBatch = bigintFilter.filter(oldRowBatch, schema);
         System.out.println("newRowBatch");
@@ -88,7 +109,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testExprTreeFilterRowBatchWithAndOr() {
+    public void testExprTreeFilterRowBatchWithAndOr()
+    {
         VectorizedRowBatch newRowBatchAnd = exprTree3.filter(oldRowBatch, schema);
         System.out.println("newRowBatchAnd");
         System.out.println(newRowBatchAnd);
@@ -98,7 +120,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testExprTreeToAndFromJson() {
+    public void testExprTreeToAndFromJson()
+    {
         Gson gson = new Gson();
         String jsonStr = gson.toJson(bigintFilter);
         ExprTree tree = gson.fromJson(jsonStr, ExprTree.class);
@@ -109,7 +132,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testLambdaEventToAndFromJson() {
+    public void testLambdaEventToAndFromJson()
+    {
         Gson gson = new Gson();
 //        LambdaEvent lambdaEvent = new LambdaEvent(filesToScan, cols, gson.toJson(bigintFilter));
 //        ExprTree filter = lambdaEvent.
@@ -119,7 +143,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testEmptyFilter() {
+    public void testEmptyFilter()
+    {
         emptyFilter.prepare(schema);
         VectorizedRowBatch newRowBatch = emptyFilter.filter(oldRowBatch, schema);
         System.out.println("newRowBatch");
@@ -127,7 +152,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testStrFilter(){
+    public void testStrFilter()
+    {
         strFilter.prepare(schema);
         VectorizedRowBatch newRowBatch = strFilter.filter(oldRowBatch, schema);
         System.out.println("newRowBatch");
@@ -145,7 +171,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testDateFilter() {
+    public void testDateFilter()
+    {
         ExprTree dateFilter = new ExprTree("ddd", ExprTree.Operator.GT, "1981-03-01");
         dateFilter.prepare(schema);
         VectorizedRowBatch newRowBatch = dateFilter.filter(oldRowBatch, schema);
@@ -154,7 +181,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testTwoLevelFilter() {
+    public void testTwoLevelFilter()
+    {
 
         twoLevelFilter.prepare(schema);
         VectorizedRowBatch newRowBatch = twoLevelFilter.filter(oldRowBatch, schema);
@@ -163,7 +191,8 @@ public class ExprTreeTest {
     }
 
     @Test
-    public void testDecimalFilter() {
+    public void testDecimalFilter()
+    {
 
         ExprTree dateFilter = new ExprTree("eee", ExprTree.Operator.GE, "5.30");
         dateFilter.prepare(schema);
