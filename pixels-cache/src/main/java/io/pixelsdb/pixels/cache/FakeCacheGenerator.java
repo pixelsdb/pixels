b34d6b3f22ab4ed3ac050a778294284363da8c93
yeeef <519689135@qq.com>
2022-05-21 17:35:27 +0200

    add FakeCacheGenerator

---
+++ pixels-cache/src/main/java/io/pixelsdb/pixels/cache/FakeCacheGenerator.java
@@ -0,0 +1,70 @@
+package io.pixelsdb.pixels.cache;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// generate fake cache data based on the cache index
+public class FakeCacheGenerator {
+    List<PixelsCacheKey> pixelsCacheKeys;
+    List<PixelsCacheIdx> pixelsCacheIdxs;
+
+
+    FakeCacheGenerator(String serializeFileName) throws IOException {
+        pixelsCacheKeys = new ArrayList<>();
+        pixelsCacheIdxs = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(serializeFileName));
+        String line = br.readLine();
+        String keyString = "";
+        String idxString = "";
+        while (line != null) {
+            keyString = line.split(";")[1];
+            idxString = line.split(";")[2];
+            String[] keyTokens = keyString.split("-");
+            long blockId = Long.parseLong(keyTokens[0]);
+            short rowGroupId = Short.parseShort(keyTokens[1]);
+            short columnId = Short.parseShort(keyTokens[2]);
+
+            String[] idxTokens = idxString.split("-");
+            long offset = Long.parseLong(idxTokens[0]);
+            int length = Integer.parseInt(idxTokens[1]);
+            pixelsCacheKeys.add(new PixelsCacheKey(blockId, rowGroupId, columnId));
+            pixelsCacheIdxs.add(new PixelsCacheIdx(offset, length));
+
+            line = br.readLine();
+        }
+        System.out.println(pixelsCacheKeys.subList(0, 10));
+        System.out.println(pixelsCacheIdxs.subList(0, 10));
+
+
+
+//        System.out.println(Arrays.toString(Arrays.copyOfRange(pixelsCacheKeys, 0, 10)));
+    }
+
+    public void gen(String outputName) throws IOException {
+        RandomAccessFile out = new RandomAccessFile(outputName, "rw");
+        int fakeIdx = 0;
+        byte[] writeBuf = new byte[4096];
+        for (int i = 0; i < pixelsCacheIdxs.size(); ++i) {
+            char fakeValue = (char) ('A' + fakeIdx);
+            PixelsCacheIdx idx = pixelsCacheIdxs.get(i);
+            out.seek(idx.offset);
+            if (idx.length > writeBuf.length) writeBuf = new byte[idx.length];
+            Arrays.fill(writeBuf, 0, idx.length, (byte) fakeValue);
+            out.write(writeBuf, 0, idx.length);
+            fakeIdx = (fakeIdx + 1) % 26;
+        }
+
+        out.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        FakeCacheGenerator gen = new FakeCacheGenerator("pixels-cache/tmp.txt");
+        gen.gen("/mnt/nvme1n1/pixels.cache");
+    }
+
+
+}
