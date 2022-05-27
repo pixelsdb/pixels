50f87211f3e70d3fab1a79e3ff1821913281be78
mzp0514 <mzp0514@gmail.com>
2022-05-27 14:40:46 -0700

    retina cpp

--- pixels-retina/src/main/java/io/pixelsdb/pixels/retina/RetinaWriter.java
+++ pixels-retina/src/main/java/io/pixelsdb/pixels/retina/RetinaWriter.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017-2019 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 package io.pixelsdb.pixels.retina;
 
 import io.pixelsdb.pixels.cache.MemoryMappedFile;
@@ -22,6 +41,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
+
+/**
+ * @author mzp0514
+ * @date 27/05/2022
+ */
+
 
 public class RetinaWriter {
 
--- pixels-retina/src/main/java/io/pixelsdb/pixels/retina/RetinaWriterServer.java
+++ pixels-retina/src/main/java/io/pixelsdb/pixels/retina/RetinaWriterServer.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2017-2019 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 package io.pixelsdb.pixels.retina;
 
 import io.grpc.stub.StreamObserver;
@@ -13,6 +32,12 @@ import io.grpc.ServerBuilder;
 
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+
+/**
+ * @author mzp0514
+ * @date 27/05/2022
+ */
+
 
 /**
  * Server that manages startup/shutdown of a {@code Greeter} server.
