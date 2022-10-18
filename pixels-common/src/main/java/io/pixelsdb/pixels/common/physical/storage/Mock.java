1b63bfca8a09aa90a91e46047f5a39b53bd5c165
Haoqiong Bian <bianhaoqiong@gmail.com>
2022-10-18 13:15:11 +0200

    [Issue #357]: fix compilation and dependency problem. (#358)
    Grpc and protobuf are upgraded to the versions used in google cloud client.

--- pixels-common/src/main/java/io/pixelsdb/pixels/common/physical/storage/Mock.java
+++ pixels-common/src/main/java/io/pixelsdb/pixels/common/physical/storage/Mock.java
@@ -1,5 +1,6 @@
 package io.pixelsdb.pixels.common.physical.storage;
 
+import io.pixelsdb.pixels.common.physical.Location;
 import io.pixelsdb.pixels.common.physical.Status;
 import io.pixelsdb.pixels.common.physical.Storage;
 
@@ -8,94 +9,137 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.List;
 
-public class Mock implements Storage {
+public class Mock implements Storage
+{
 
-    public Mock() {
+    public Mock()
+    {
         // read the dumpedCache.txt
     }
 
     @Override
-    public Scheme getScheme() {
+    public Scheme getScheme()
+    {
         return Storage.Scheme.mock;
     }
 
     @Override
-    public String ensureSchemePrefix(String path) throws IOException {
+    public String ensureSchemePrefix(String path) throws IOException
+    {
         return path;
     }
 
     @Override
-    public List<Status> listStatus(String path) throws IOException {
+    public List<Status> listStatus(String path) throws IOException
+    {
         return null;
     }
 
     @Override
-    public List<String> listPaths(String path) throws IOException {
+    public List<String> listPaths(String path) throws IOException
+    {
         return null;
     }
 
     @Override
-    public Status getStatus(String path) throws IOException {
+    public Status getStatus(String path) throws IOException
+    {
         return null;
     }
 
     @Override
-    public long getFileId(String path) throws IOException {
+    public long getFileId(String path) throws IOException
+    {
         return 0;
     }
 
     @Override
-    public boolean mkdirs(String path) throws IOException {
+    public boolean hasLocality()
+    {
+        return Storage.super.hasLocality();
+    }
+
+    @Override
+    public List<Location> getLocations(String path) throws IOException
+    {
+        return Storage.super.getLocations(path);
+    }
+
+    @Override
+    public String[] getHosts(String path) throws IOException
+    {
+        return Storage.super.getHosts(path);
+    }
+
+    @Override
+    public boolean mkdirs(String path) throws IOException
+    {
         return false;
     }
 
     @Override
-    public DataInputStream open(String path) throws IOException {
+    public DataInputStream open(String path) throws IOException
+    {
         return null;
     }
 
     @Override
-    public DataOutputStream create(String path, boolean overwrite, int bufferSize, short replication) throws IOException {
+    public DataOutputStream create(String path, boolean overwrite, int bufferSize) throws IOException
+    {
         return null;
     }
 
     @Override
-    public DataOutputStream create(String path, boolean overwrite, int bufferSize, short replication, long blockSize) throws IOException {
+    public DataOutputStream create(String path, boolean overwrite, int bufferSize, short replication) throws IOException
+    {
         return null;
     }
 
     @Override
-    public boolean delete(String path, boolean recursive) throws IOException {
+    public DataOutputStream create(String path, boolean overwrite, int bufferSize, short replication, long blockSize) throws IOException
+    {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String path, boolean recursive) throws IOException
+    {
         return false;
     }
 
     @Override
-    public boolean supportDirectCopy() {
+    public boolean supportDirectCopy()
+    {
         return false;
     }
 
     @Override
-    public boolean directCopy(String src, String dest) throws IOException {
+    public boolean directCopy(String src, String dest) throws IOException
+    {
         return false;
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() throws IOException
+    {
 
     }
 
     @Override
-    public boolean exists(String path) throws IOException {
+    public boolean exists(String path) throws IOException
+    {
         return false;
     }
 
     @Override
-    public boolean isFile(String path) throws IOException {
+    public boolean isFile(String path) throws IOException
+    {
         return false;
     }
 
     @Override
-    public boolean isDirectory(String path) throws IOException {
+    public boolean isDirectory(String path) throws IOException
+    {
         return false;
     }
 }
