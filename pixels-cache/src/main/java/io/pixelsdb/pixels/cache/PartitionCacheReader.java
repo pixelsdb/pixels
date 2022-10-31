67cb83253731e9a361d485dbcc516bf1538101b7
Haoqiong Bian <bianhaoqiong@gmail.com>
2022-10-31 13:19:33 +0100

    [Issue #170]: refine query queues. (#361)

--- pixels-cache/src/main/java/io/pixelsdb/pixels/cache/PartitionCacheReader.java
+++ pixels-cache/src/main/java/io/pixelsdb/pixels/cache/PartitionCacheReader.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2022 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 package io.pixelsdb.pixels.cache;
 
 import io.pixelsdb.pixels.common.utils.ConfigFactory;
@@ -9,7 +28,6 @@ import java.nio.ByteBuffer;
 import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
--- pixels-cache/src/main/java/io/pixelsdb/pixels/cache/RadixIndexWriter.java
+++ pixels-cache/src/main/java/io/pixelsdb/pixels/cache/RadixIndexWriter.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright 2022 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 package io.pixelsdb.pixels.cache;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.nio.ByteBuffer;
-
-import static com.google.common.base.Preconditions.checkArgument;
 
 // serialize to a memory mapped file
 public class RadixIndexWriter implements CacheIndexWriter {
