5c0bf23858c35977ff3f3efe9de75500ac0dea7f
Bian Haoqiong <bianhaoqiong@gmail.com>
2023-04-19 12:42:21 +0200

    init project for pixels-proxy.

---
+++ pixels-proxy/src/main/java/io/pixelsdb/pixels/proxy/PixelsProxyApplication.java
@@ -0,0 +1,13 @@
+package io.pixelsdb.pixels.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelsProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelsProxyApplication.class, args);
+	}
+
+}
---
+++ pixels-proxy/src/test/java/io/pixelsdb/pixels/proxy/PixelsProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pixelsdb.pixels.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelsProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
