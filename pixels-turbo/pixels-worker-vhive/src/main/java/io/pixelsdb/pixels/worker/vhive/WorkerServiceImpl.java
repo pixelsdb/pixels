9d5062b5e0b5aa90cb41cb88e19b62b46de8f694
zhaoshihan <zhaosh1997@gmail.com>
2023-04-21 16:30:02 +0200

    feat: implement conversion

--- pixels-turbo/pixels-worker-vhive/src/main/java/io/pixelsdb/pixels/worker/vhive/WorkerServiceImpl.java
+++ pixels-turbo/pixels-worker-vhive/src/main/java/io/pixelsdb/pixels/worker/vhive/WorkerServiceImpl.java
@@ -4,7 +4,8 @@ import com.google.common.primitives.Booleans;
 import com.google.common.primitives.Ints;
 import io.grpc.stub.StreamObserver;
 
-import io.pixelsdb.pixels.common.turbo.Input;
+import io.pixelsdb.pixels.common.physical.Storage;
+import io.pixelsdb.pixels.common.turbo.Output;
 import io.pixelsdb.pixels.executor.aggregation.FunctionType;
 import io.pixelsdb.pixels.planner.plan.physical.domain.*;
 import io.pixelsdb.pixels.planner.plan.physical.input.ScanInput;
@@ -23,79 +24,128 @@ public class WorkerServiceImpl extends WorkerServiceGrpc.WorkerServiceImplBase {
     }
 
     @Override
-    public void scan(WorkerProto.ScanRequest request, StreamObserver<WorkerProto.ScanResponse> responseObserver) {
-        WorkerProto.ScanResponse response;
-        List<InputSplit> inputSplits = request.getScanTableInfo().getInputSplitsList().stream()
-                .map(outsideItem -> new InputSplit(outsideItem.getInputInfosList().stream()
-                        .map(insideItem -> InputInfo.newBuilder()
-                                .setPath(insideItem.getPath())
-                                .setRgStart(insideItem.getRgStart())
-                                .setRgLength(insideItem.getRgLength())
-                                .build())
-                        .collect(Collectors.toList())) )
-                .collect(Collectors.toList());
+    public void scan(WorkerProto.ScanInput request, StreamObserver<WorkerProto.ScanOutput> responseObserver) {
+        ScanOutput output = this.scanWorker.handleRequest(protoToPhysical(request));
 
-        ScanTableInfo scanTableInfo = ScanTableInfo.newBuilder()
-                .setTableName(request.getScanTableInfo().getTableName())
-                .setBase(request.getScanTableInfo().getBase())
-                .setColumnsToRead(request.getScanTableInfo().getColumnsToReadList().toArray(new String[0]))
-                .setInputSplits(inputSplits)
-                .setFilter(request.getScanTableInfo().getFilter())
-                .build();
-
-        FunctionType[] functionTypes = request.getPartialAggregationInfo().getFunctionTypesList().stream()
-                .map(item -> FunctionType.fromNumber(item.getNumber()))
-                .toArray(FunctionType[]::new);
-
-        PartialAggregationInfo partialAggregationInfo = PartialAggregationInfo.newBuilder()
-                .setGroupKeyColumnAlias(request.getPartialAggregationInfo().getGroupKeyColumnAliasList().toArray(new String[0]))
-                .setResultColumnAlias(request.getPartialAggregationInfo().getResultColumnAliasList().toArray(new String[0]))
-                .setResultColumnTypes(request.getPartialAggregationInfo().getResultColumnTypesList().toArray(new String[0]))
-                .setGroupKeyColumnIds(Ints.toArray(request.getPartialAggregationInfo().getGroupKeyColumnIdsList()))
-                .setAggregateColumnIds(Ints.toArray(request.getPartialAggregationInfo().getAggregateColumnIdsList()))
-                .setFunctionTypes(functionTypes)
-                .setPartition(request.getPartialAggregationInfo().getPartition())
-                .setNumPartition(request.getPartialAggregationInfo().getNumPartition())
-                .build();
-
-        StorageInfo storageInfo = StorageInfo.newBuilder()
-                .
-                .build();
-
-        OutputInfo outputInfo = OutputInfo.newBuilder()
-                .setPath(request.getOutput().getPath())
-                .setRandomFileName(request.getOutput().getRandomFileName())
-                .setStorageInfo()
-                .setEncoding()
-                .build();
-
-        ScanInput input = ScanInput.newBuilder()
-                .setQueryId(request.getQueryId())
-                .setScanTableInfo(scanTableInfo)
-                .setScanProjection(Booleans.toArray(request.getScanProjectionList()))
-                .setPartialAggregationPresent(request.getPartialAggregationPresent())
-                .setPartialAggregationInfo(partialAggregationInfo)
-                .setOutput()
-                .build();
-
-        ScanOutput output = this.handler.handleRequest(input);
-
-//        super.scan(request, responseObserver);
-
+        WorkerProto.ScanOutput response = physicalToProto(output);
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 
     @Override
     public void hello(WorkerProto.HelloRequest request, StreamObserver<WorkerProto.HelloResponse> responseObserver) {
-        WorkerProto.HelloResponse response;
-
         String output = "Hello, " + request.getName();
-        response = WorkerProto.HelloResponse.newBuilder().setOutput(output).build();
 
+        WorkerProto.HelloResponse response = WorkerProto.HelloResponse.newBuilder().setOutput(output).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 
+    private ScanInput protoToPhysical(WorkerProto.ScanInput request) {
+        return ScanInput.newBuilder()
+                .setQueryId(request.getQueryId())
+                .setScanTableInfo(protoToPhysical(request.getScanTableInfo()))
+                .setScanProjection(Booleans.toArray(request.getScanProjectionList()))
+                .setPartialAggregationPresent(request.getPartialAggregationPresent())
+                .setPartialAggregationInfo(protoToPhysical(request.getPartialAggregationInfo()))
+                .setOutput(protoToPhysical(request.getOutput()))
+                .build();
+    }
 
+    private ScanTableInfo protoToPhysical(WorkerProto.ScanTableInfo request) {
+        return ScanTableInfo.newBuilder()
+                .setTableName(request.getTableName())
+                .setBase(request.getBase())
+                .setColumnsToRead(request.getColumnsToReadList().toArray(new String[0]))
+                .setInputSplits(
+                        request.getInputSplitsList().stream()
+                        .map(this::protoToPhysical)
+                        .collect(Collectors.toList())
+                )
+                .setFilter(request.getFilter())
+                .build();
+    }
+
+    private InputSplit protoToPhysical(WorkerProto.InputSplit request) {
+        return new InputSplit(request.getInputInfosList().stream()
+                .map(this::protoToPhysical)
+                .collect(Collectors.toList()));
+    }
+
+    private InputInfo protoToPhysical(WorkerProto.InputInfo request) {
+        return InputInfo.newBuilder()
+                .setPath(request.getPath())
+                .setRgStart(request.getRgStart())
+                .setRgLength(request.getRgLength())
+                .build();
+    }
+
+    private PartialAggregationInfo protoToPhysical(WorkerProto.PartialAggregationInfo request) {
+        return PartialAggregationInfo.newBuilder()
+                .setGroupKeyColumnAlias(request.getGroupKeyColumnAliasList().toArray(new String[0]))
+                .setResultColumnAlias(request.getResultColumnAliasList().toArray(new String[0]))
+                .setResultColumnTypes(request.getResultColumnTypesList().toArray(new String[0]))
+                .setGroupKeyColumnIds(Ints.toArray(request.getGroupKeyColumnIdsList()))
+                .setAggregateColumnIds(Ints.toArray(request.getAggregateColumnIdsList()))
+                .setFunctionTypes(
+                        request.getFunctionTypesList().stream()
+                        .map(this::protoToPhysical)
+                        .toArray(FunctionType[]::new)
+                )
+                .setPartition(request.getPartition())
+                .setNumPartition(request.getNumPartition())
+                .build();
+    }
+
+    private FunctionType protoToPhysical(WorkerProto.FunctionType request) {
+        return FunctionType.fromNumber(request.getNumber());
+    }
+
+    private OutputInfo protoToPhysical(WorkerProto.OutputInfo request) {
+        return OutputInfo.newBuilder()
+                .setPath(request.getPath())
+                .setRandomFileName(request.getRandomFileName())
+                .setStorageInfo(protoToPhysical(request.getStorageInfo()))
+                .setEncoding(request.getEncoding())
+                .build();
+    }
+
+    private StorageInfo protoToPhysical(WorkerProto.StorageInfo request) {
+        return StorageInfo.newBuilder()
+                .setScheme(protoToPhysical(request.getScheme()))
+                .setEndpoint(request.getEndpoint())
+                .setAccessKey(request.getAccessKey())
+                .setSecretKey(request.getSecretKey())
+                .build();
+    }
+
+    private Storage.Scheme protoToPhysical(WorkerProto.Scheme request) {
+        return Storage.Scheme.fromNumber(request.getNumber());
+    }
+
+    private WorkerProto.ScanOutput physicalToProto(ScanOutput response) {
+        return WorkerProto.ScanOutput.newBuilder()
+                .setBase(physicalToProto((Output)response))
+                .addAllOutputs(response.getOutputs())
+                .addAllRowGroupNums(response.getRowGroupNums())
+                .build();
+    }
+
+    private WorkerProto.Output physicalToProto(Output response) {
+        return WorkerProto.Output.newBuilder()
+                .setRequestId(response.getRequestId())
+                .setSuccessful(response.isSuccessful())
+                .setErrorMessage(response.getErrorMessage())
+                .setStartTimeMs(response.getStartTimeMs())
+                .setDurationMs(response.getDurationMs())
+                .setMemoryMB(response.getMemoryMB())
+                .setCumulativeInputCostMs(response.getCumulativeInputCostMs())
+                .setCumulativeComputeCostMs(response.getCumulativeComputeCostMs())
+                .setCumulativeOutputCostMs(response.getCumulativeOutputCostMs())
+                .setNumReadRequests(response.getNumReadRequests())
+                .setNumWriteRequests(response.getNumWriteRequests())
+                .setTotalReadBytes(response.getTotalReadBytes())
+                .setTotalWriteBytes(response.getTotalWriteBytes())
+                .build();
+    }
 }
