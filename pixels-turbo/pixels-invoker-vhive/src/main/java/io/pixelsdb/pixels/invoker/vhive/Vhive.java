2bbf5c7df4de950288565ced3cb6a3340af7138a
zhaoshihan <zhaosh1997@gmail.com>
2023-05-13 16:17:55 +0200

    feat: add vhive invoker to turbo

--- pixels-turbo/pixels-invoker-vhive/src/main/java/io/pixelsdb/pixels/invoker/vhive/Vhive.java
+++ pixels-turbo/pixels-invoker-vhive/src/main/java/io/pixelsdb/pixels/invoker/vhive/Vhive.java
@@ -1,18 +1,22 @@
 package io.pixelsdb.pixels.invoker.vhive;
 
+import io.pixelsdb.pixels.common.utils.ConfigFactory;
+
 public class Vhive {
+    private static final ConfigFactory config = ConfigFactory.Instance();
     private static final Vhive instance = new Vhive();
-
-    public static Vhive Instance() {
-        return instance;
-    }
-
     private final WorkerSyncClient client;
     private final WorkerAsyncClient asyncClient;
 
     private Vhive() {
-        client = new WorkerSyncClient();
-        asyncClient = new WorkerAsyncClient();
+        String hostname = config.getProperty("vhive.hostname");
+        int port = Integer.parseInt(config.getProperty("vhive.port"));
+        client = new WorkerSyncClient(hostname, port);
+        asyncClient = new WorkerAsyncClient(hostname, port);
+    }
+
+    public static Vhive Instance() {
+        return instance;
     }
 
     public WorkerSyncClient getSyncClient() {
