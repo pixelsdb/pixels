1cfa34bc5fee571ec0bde589c96f1f9cc382752a
Lin Yuan <voidforall@gmail.com>
2023-05-18 20:44:28 +0200

    Add yaml configuration

--- pixels-turbo/pixels-worker-amphi/main.cpp
+++ pixels-turbo/pixels-worker-amphi/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
+
 #include "duckdb.hpp"
+#include "yaml-cpp/yaml.h"
+
 #include "worker/include/grpc/transpile_sql_client.h"
 
 int main() {
+    YAML::Node config = YAML::LoadFile("config.yaml");
+
     // DuckDB example
     duckdb::DuckDB db(nullptr);
     duckdb::Connection con(db);
@@ -12,12 +18,13 @@ int main() {
     result->Print();
 
     // gRPC example
-    std::string host_addr = "ec2-18-218-128-203.us-east-2.compute.amazonaws.com:18892";
-    TranspileSqlClient client(grpc::CreateChannel(host_addr, grpc::InsecureChannelCredentials()));
+    std::string host_addr = config["server_address"].as<std::string>();
+    std::string port = config["server_port"].as<std::string>();
+    TranspileSqlClient client(grpc::CreateChannel(host_addr + ":" + port, grpc::InsecureChannelCredentials()));
     std::string token = "";
     std::string sql_statement = "SELECT STRFTIME(x, '%y-%-m-%S');";
-    std::string from_dialect = "duckdb";
-    std::string to_dialect = "hive";
+    std::string from_dialect = config["local_engine"].as<std::string>();
+    std::string to_dialect = config["remote_engine"].as<std::string>();
 
     std::string transpiled_sql = client.TranspileSql(token, sql_statement, from_dialect, to_dialect);
 
