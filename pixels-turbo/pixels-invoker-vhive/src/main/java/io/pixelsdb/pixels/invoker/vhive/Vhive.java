fa063c0e71ecd4b824395c34ad2edec17dc5a1af
zhaoshihan <zhaosh1997@gmail.com>
2023-05-21 15:51:44 +0200

    fix: remove sync client

--- pixels-turbo/pixels-invoker-vhive/src/main/java/io/pixelsdb/pixels/invoker/vhive/Vhive.java
+++ pixels-turbo/pixels-invoker-vhive/src/main/java/io/pixelsdb/pixels/invoker/vhive/Vhive.java
@@ -5,22 +5,16 @@ import io.pixelsdb.pixels.common.utils.ConfigFactory;
 public class Vhive {
     private static final ConfigFactory config = ConfigFactory.Instance();
     private static final Vhive instance = new Vhive();
-    private final WorkerSyncClient client;
     private final WorkerAsyncClient asyncClient;
 
     private Vhive() {
         String hostname = config.getProperty("vhive.hostname");
         int port = Integer.parseInt(config.getProperty("vhive.port"));
-        client = new WorkerSyncClient(hostname, port);
         asyncClient = new WorkerAsyncClient(hostname, port);
     }
 
     public static Vhive Instance() {
         return instance;
-    }
-
-    public WorkerSyncClient getSyncClient() {
-        return client;
     }
 
     public WorkerAsyncClient getAsyncClient() {
