1edc98bcf5c77b220233d29c7142cab3c4747847
zhaoshihan <zhaosh1997@gmail.com>
2023-05-23 18:03:13 +0200

    feat: create sleep module

---
+++ pixels-turbo/pixels-worker-sleep/src/main/java/io/pixelsdb/pixels/worker/sleep/App.java
@@ -0,0 +1,10 @@
+package io.pixelsdb.pixels.worker.sleep;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
