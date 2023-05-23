72d0634ab93a196d82995d6ff53f212d3d4a1b68
zhaoshihan <zhaosh1997@gmail.com>
2023-05-23 17:05:31 +0200

    feat: add async profiler

--- pixels-turbo/pixels-worker-vhive/src/main/java/io/pixelsdb/pixels/worker/vhive/utils/ServiceImpl.java
+++ pixels-turbo/pixels-worker-vhive/src/main/java/io/pixelsdb/pixels/worker/vhive/utils/ServiceImpl.java
@@ -2,9 +2,18 @@ package io.pixelsdb.pixels.worker.vhive.utils;
 
 import com.alibaba.fastjson.JSON;
 import io.grpc.stub.StreamObserver;
+import io.pixelsdb.pixels.common.turbo.Input;
+import io.pixelsdb.pixels.common.turbo.Output;
+import io.pixelsdb.pixels.common.utils.ConfigFactory;
 import io.pixelsdb.pixels.worker.common.WorkerProto;
+import one.profiler.AsyncProfiler;
+import org.apache.commons.net.ftp.FTPClient;
 
-public class ServiceImpl <I, O> {
+import java.io.FileInputStream;
+
+public class ServiceImpl<I extends Input, O extends Output> {
+    private static final ConfigFactory config = ConfigFactory.Instance();
+    private static final AsyncProfiler profiler = AsyncProfiler.getInstance();
     final RequestHandler<I, O> handler;
     final Class<I> typeParameterClass;
 
@@ -19,7 +28,34 @@ public class ServiceImpl <I, O> {
     public void execute(WorkerProto.WorkerRequest request,
                         StreamObserver<WorkerProto.WorkerResponse> responseObserver) {
         I input = JSON.parseObject(request.getJson(), typeParameterClass);
-        O output = handler.handleRequest(input);
+        O output;
+
+        boolean isProfile = Boolean.parseBoolean(config.getProperty("vhive.profiling.enabled"));
+        try {
+            if (isProfile) {
+                String fileName = "myprofile.jfr";
+                profiler.execute(String.format("start,jfr,threads,event=wall,file=%s", fileName));
+                output = handler.handleRequest(input);
+//            Thread.sleep(60 * 1000);
+                profiler.execute(String.format("stop,file=%s", fileName));
+
+                // store the JFR profiling file to FTP server
+                String hostname = config.getProperty("vhive.profiling.ftp.hostname");
+                int port = Integer.parseInt(config.getProperty("vhive.profiling.ftp.port"));
+                FTPClient ftpClient = new FTPClient();
+                ftpClient.connect(hostname, port);
+                String username = config.getProperty("vhive.profiling.ftp.username");
+                String password = config.getProperty("vhive.profiling.ftp.password");
+                ftpClient.login(username, password);
+                FileInputStream inputStream = new FileInputStream(fileName);
+                ftpClient.storeFile("experiments/" + fileName, inputStream);
+                ftpClient.logout();
+            } else {
+                output = handler.handleRequest(input);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Exception during process: ", e);
+        }
 
         WorkerProto.WorkerResponse response = WorkerProto.WorkerResponse.newBuilder()
                 .setJson(JSON.toJSONString(output))
