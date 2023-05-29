101d7c81f109e6c3810b7560b82b0fcc70da4e9f
Haoqiong Bian <bianhaoqiong@gmail.com>
2023-05-29 18:28:46 +0200

    [Issue #431] update docs and add file headers. (#461)

--- pixels-parser/src/test/java/io/pixelsdb/pixels/parser/TestPixelsSchema.java
+++ pixels-parser/src/test/java/io/pixelsdb/pixels/parser/TestPixelsSchema.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2023 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 package io.pixelsdb.pixels.parser;
 
 import io.pixelsdb.pixels.common.exception.MetadataException;
@@ -7,19 +26,13 @@ import org.apache.calcite.rel.type.RelDataTypeField;
 import org.apache.calcite.rel.type.RelDataTypeSystem;
 import org.apache.calcite.sql.type.SqlTypeFactoryImpl;
 import org.apache.calcite.sql.type.SqlTypeName;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 public class TestPixelsSchema
 {
