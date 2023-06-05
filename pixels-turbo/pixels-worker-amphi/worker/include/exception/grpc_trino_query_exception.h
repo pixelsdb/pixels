91b3fc20e7363d38a28c978714dd35f0b4542a08
Lin Yuan <voidforall@gmail.com>
2023-06-05 17:35:51 +0200

    update license in amphi worker files

--- pixels-turbo/pixels-worker-amphi/worker/include/exception/grpc_trino_query_exception.h
+++ pixels-turbo/pixels-worker-amphi/worker/include/exception/grpc_trino_query_exception.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2023 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 #ifndef PIXELS_WORKER_AMPHI_GRPC_TRINO_QUERY_EXCEPTION_H
 #define PIXELS_WORKER_AMPHI_GRPC_TRINO_QUERY_EXCEPTION_H
 
