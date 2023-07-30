be99e047aad55ae62969f4a00a054a34999cf50b
yuly16 <41314695+yuly16@users.noreply.github.com>
2023-07-30 14:35:55 +0200

    [Issue #514] change pixels c++ column vector from 4k alignment to 32 byte alignment (#515)

--- cpp/pixels-reader/pixels-core/lib/vector/DateColumnVector.cpp
+++ cpp/pixels-reader/pixels-core/lib/vector/DateColumnVector.cpp
@@ -6,7 +6,7 @@
 
 DateColumnVector::DateColumnVector(int len, bool encoding): ColumnVector(len, encoding) {
 	if(encoding) {
-        posix_memalign(reinterpret_cast<void **>(&dates), 4096,
+        posix_memalign(reinterpret_cast<void **>(&dates), 32,
                        len * sizeof(int32_t));
 	} else {
 		this->dates = nullptr;
