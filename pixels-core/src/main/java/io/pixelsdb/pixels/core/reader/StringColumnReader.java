980a5cc6ce371a38c22ec50029f0d5cda9e08e96
Bian Haoqiong <bianhaoqiong@gmail.com>
2023-08-04 08:58:20 +0200

    fix string column reader

--- pixels-core/src/main/java/io/pixelsdb/pixels/core/reader/StringColumnReader.java
+++ pixels-core/src/main/java/io/pixelsdb/pixels/core/reader/StringColumnReader.java
@@ -156,7 +156,7 @@ public class StringColumnReader extends ColumnReader
             // no memory copy
             inputBuffer = Unpooled.wrappedBuffer(input);
             boolean littleEndian = chunkIndex.hasLittleEndian() && chunkIndex.getLittleEndian();
-            inputBuffer.order(littleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+            inputBuffer = inputBuffer.order(littleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
             readContent(input.remaining(), encoding);
             isNullOffset = (int) chunkIndex.getIsNullOffset();
             bufferOffset = 0;
