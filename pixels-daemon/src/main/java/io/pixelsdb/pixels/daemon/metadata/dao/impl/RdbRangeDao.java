82c1cda484c02de6b8f63c060e233ea7c2611a4f
Bian Haoqiong <bianhaoqiong@gmail.com>
2024-05-29 00:54:18 +0800

    implement dao

--- pixels-daemon/src/main/java/io/pixelsdb/pixels/daemon/metadata/dao/impl/RdbRangeDao.java
+++ pixels-daemon/src/main/java/io/pixelsdb/pixels/daemon/metadata/dao/impl/RdbRangeDao.java
@@ -26,10 +26,8 @@ import io.pixelsdb.pixels.daemon.metadata.dao.RangeDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -73,30 +71,138 @@ public class RdbRangeDao extends RangeDao
     @Override
     public List<MetadataProto.Range> getAllByRangeIndexId(long rangeIndexId)
     {
+        Connection conn = db.getConnection();
+        try (Statement st = conn.createStatement())
+        {
+            ResultSet rs = st.executeQuery("SELECT * FROM RANGES WHERE RANGE_INDEXES_RI_ID=" + rangeIndexId);
+            List<MetadataProto.Range> ranges = new ArrayList<>();
+            while (rs.next())
+            {
+                MetadataProto.Range range = MetadataProto.Range.newBuilder()
+                        .setId(rs.getLong("RANGE_ID"))
+                        .setMin(ByteString.copyFrom(rs.getBytes("RANGE_MIN")))
+                        .setMax(ByteString.copyFrom(rs.getBytes("RANGE_MAX")))
+                        // Issue #658: parent id is set to 0 if it is null in metadata.
+                        .setParentId(rs.getLong("RANGE_PARENT_ID"))
+                        .setRangeIndexId(rangeIndexId).build();
+                ranges.add(range);
+            }
+            return ranges;
+        } catch (SQLException e)
+        {
+            log.error("getAllByRangeIndexId in RdbRangeDao", e);
+        }
+
         return null;
     }
 
     @Override
     public boolean exists(MetadataProto.Range range)
     {
+        Connection conn = db.getConnection();
+        try (Statement st = conn.createStatement())
+        {
+            String sql = "SELECT 1 FROM RANGES WHERE RANGE_ID=" + range.getId();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next())
+            {
+                return true;
+            }
+        } catch (SQLException e)
+        {
+            log.error("exists in RdbRangeDao", e);
+        }
+
         return false;
     }
 
     @Override
     public long insert(MetadataProto.Range range)
     {
-        return 0;
+        Connection conn = db.getConnection();
+        String sql = "INSERT INTO RANGES(" +
+                "`RANGE_MIN`," +
+                "`RANGE_MAX`," +
+                "`RANGE_PARENT_ID`," +
+                "`RANGE_INDEXES_RI_ID`) VALUES (?,?,?,?)";
+        try (PreparedStatement pst = conn.prepareStatement(sql))
+        {
+            pst.setBytes(1, range.getMin().toByteArray());
+            pst.setBytes(2, range.getMax().toByteArray());
+            if (range.hasParentId())
+            {
+                pst.setLong(3, range.getParentId());
+            }
+            else
+            {
+                pst.setNull(3, Types.BIGINT);
+            }
+            pst.setLong(4, range.getRangeIndexId());
+            if (pst.executeUpdate() == 1)
+            {
+                ResultSet rs = pst.executeQuery("SELECT LAST_INSERT_ID()");
+                if (rs.next())
+                {
+                    return rs.getLong(1);
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        } catch (SQLException e)
+        {
+            log.error("insert in RdbRangeDao", e);
+        }
+
+        return -1;
     }
 
     @Override
     public boolean update(MetadataProto.Range range)
     {
+        Connection conn = db.getConnection();
+        String sql = "UPDATE RANGES\n" +
+                "SET\n" +
+                "`RANGE_MIN` = ?," +
+                "`RANGE_MXN` = ?," +
+                "`RANGE_PARENT_ID` = ?," +
+                "`RANGE_INDEXES_RI_ID` = ?\n" +
+                "WHERE `RANGE_ID` = ?";
+        try (PreparedStatement pst = conn.prepareStatement(sql))
+        {
+            pst.setBytes(1, range.getMin().toByteArray());
+            pst.setBytes(2, range.getMax().toByteArray());
+            pst.setLong(3, range.getParentId());
+            pst.setLong(4, range.getRangeIndexId());
+            pst.setLong(5, range.getId());
+            return pst.executeUpdate() == 1;
+        } catch (SQLException e)
+        {
+            log.error("update in RdbRangeDao", e);
+        }
+
         return false;
     }
 
     @Override
     public boolean deleteById(long id)
     {
+        Connection conn = db.getConnection();
+        String sql = "DELETE FROM RANGES WHERE RANGE_ID=?";
+        try (PreparedStatement pst = conn.prepareStatement(sql))
+        {
+            pst.setLong(1, id);
+            return pst.executeUpdate() == 1;
+        } catch (SQLException e)
+        {
+            log.error("deleteById in RdbRangeDao", e);
+        }
+
         return false;
     }
 }
--- pixels-daemon/src/main/java/io/pixelsdb/pixels/daemon/metadata/dao/impl/RdbRangeIndexDao.java
+++ pixels-daemon/src/main/java/io/pixelsdb/pixels/daemon/metadata/dao/impl/RdbRangeIndexDao.java
@@ -26,10 +26,8 @@ import io.pixelsdb.pixels.daemon.metadata.dao.RangeIndexDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -70,38 +68,160 @@ public class RdbRangeIndexDao extends RangeIndexDao
     }
 
     @Override
-    public MetadataProto.RangeIndex getByTableAndSvIds(long tableId, long schemaVersionId)
-    {
-        return null;
-    }
-
-    @Override
     public List<MetadataProto.RangeIndex> getAllByTableId(long tableId)
     {
+        Connection conn = db.getConnection();
+        try (Statement st = conn.createStatement())
+        {
+            ResultSet rs = st.executeQuery("SELECT * FROM RANGE_INDEXES WHERE TBLS_TBL_ID=" + tableId);
+            List<MetadataProto.RangeIndex> rangeIndexes = new ArrayList<>();
+            while (rs.next())
+            {
+                MetadataProto.RangeIndex rangeIndex = MetadataProto.RangeIndex.newBuilder()
+                        .setId(rs.getLong("RI_ID"))
+                        .setIndexStruct(ByteString.copyFrom(rs.getBytes("RI_INDEX_STRUCT")))
+                        .setKeyColumns(rs.getString("RI_KEY_COLUMNS"))
+                        .setTableId(tableId)
+                        .setSchemaVersionId(rs.getLong("SCHEMA_VERSIONS_SV_ID")).build();
+                rangeIndexes.add(rangeIndex);
+            }
+            return rangeIndexes;
+        } catch (SQLException e)
+        {
+            log.error("getAllByTableId in RdbRangeIndexDao", e);
+        }
+
         return null;
     }
 
     @Override
-    public boolean exists(MetadataProto.RangeIndex path)
+    public MetadataProto.RangeIndex getByTableAndSvIds(long tableId, long schemaVersionId)
     {
+        Connection conn = db.getConnection();
+        String sql = "SELECT * FROM RANGE_INDEXES WHERE TBLS_TBL_ID=? AND SCHEMA_VERSIONS_SV_ID=?";
+        try (PreparedStatement pst = conn.prepareStatement(sql))
+        {
+            pst.setLong(1, tableId);
+            pst.setLong(2, schemaVersionId);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next())
+            {
+                MetadataProto.RangeIndex rangeIndex = MetadataProto.RangeIndex.newBuilder()
+                        .setId(rs.getLong("RI_ID"))
+                        .setIndexStruct(ByteString.copyFrom(rs.getBytes("RI_INDEX_STRUCT")))
+                        .setKeyColumns(rs.getString("RI_KEY_COLUMNS"))
+                        .setTableId(tableId)
+                        .setSchemaVersionId(schemaVersionId).build();
+                return rangeIndex;
+            }
+        } catch (SQLException e)
+        {
+            log.error("getByTableAndSvIds in RdbRangeIndexDao", e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean exists(MetadataProto.RangeIndex rangeIndex)
+    {
+        Connection conn = db.getConnection();
+        try (Statement st = conn.createStatement())
+        {
+            String sql = "SELECT 1 FROM RANGE_INDEXES WHERE RI_ID=" + rangeIndex.getId();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next())
+            {
+                return true;
+            }
+        } catch (SQLException e)
+        {
+            log.error("exists in RdbRangeIndexDao", e);
+        }
+
         return false;
     }
 
     @Override
     public long insert(MetadataProto.RangeIndex rangeIndex)
     {
-        return 0;
+        Connection conn = db.getConnection();
+        String sql = "INSERT INTO RANGE_INDEXES(" +
+                "`RI_INDEX_STRUCT`," +
+                "`RI_KEY_COLUMNS`," +
+                "`TBLS_TBL_ID`," +
+                "`SCHEMA_VERSIONS_SV_ID`) VALUES (?,?,?,?)";
+        try (PreparedStatement pst = conn.prepareStatement(sql))
+        {
+            pst.setBytes(1, rangeIndex.getIndexStruct().toByteArray());
+            pst.setString(2, rangeIndex.getKeyColumns());
+            pst.setLong(3, rangeIndex.getTableId());
+            pst.setLong(4, rangeIndex.getSchemaVersionId());
+            if (pst.executeUpdate() == 1)
+            {
+                ResultSet rs = pst.executeQuery("SELECT LAST_INSERT_ID()");
+                if (rs.next())
+                {
+                    return rs.getLong(1);
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        } catch (SQLException e)
+        {
+            log.error("insert in RdbRangeIndexDao", e);
+        }
+
+        return -1;
     }
 
     @Override
     public boolean update(MetadataProto.RangeIndex rangeIndex)
     {
+        Connection conn = db.getConnection();
+        String sql = "UPDATE RANGE_INDEXES\n" +
+                "SET\n" +
+                "`RI_INDEX_STRUCT` = ?," +
+                "`RI_KEY_COLUMNS` = ?," +
+                "`TBLS_TBL_ID` = ?," +
+                "`SCHEMA_VERSION_SV_ID` = ?\n" +
+                "WHERE `RI_ID` = ?";
+        try (PreparedStatement pst = conn.prepareStatement(sql))
+        {
+            pst.setBytes(1, rangeIndex.getIndexStruct().toByteArray());
+            pst.setString(2, rangeIndex.getKeyColumns());
+            pst.setLong(3, rangeIndex.getTableId());
+            pst.setLong(4, rangeIndex.getSchemaVersionId());
+            pst.setLong(5, rangeIndex.getId());
+            return pst.executeUpdate() == 1;
+        } catch (SQLException e)
+        {
+            log.error("update in RdbRangeIndexDao", e);
+        }
+
         return false;
     }
 
     @Override
     public boolean deleteById(long id)
     {
+        Connection conn = db.getConnection();
+        String sql = "DELETE FROM RANGE_INDEXES WHERE RI_ID=?";
+        try (PreparedStatement pst = conn.prepareStatement(sql))
+        {
+            pst.setLong(1, id);
+            return pst.executeUpdate() == 1;
+        } catch (SQLException e)
+        {
+            log.error("deleteById in RdbRangeIndexDao", e);
+        }
+
         return false;
     }
 }
