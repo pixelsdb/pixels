41ab338f50b2c6e0215a1090117ec1ad3f8dfe57
Bian Haoqiong <bianhaoqiong@gmail.com>
2024-09-17 21:34:47 +0800

    finish

--- scripts/bin/run-class.sh
+++ scripts/bin/run-class.sh
@@ -40,19 +40,6 @@ else
   JAVA="$JAVA_HOME/bin/java"
 fi
 
-# Set JVM options file path or use default JVM options
-JVM_OPTIONS_FILE="$PIXELS_HOME/bin/jvm.config"
-if [ -z "$PIXELS_JVM_OPTS" ]; then
-  if [ -e "$JVM_OPTIONS_FILE" ]; then
-    PIXELS_JVM_OPTS=$(tr '\n' ' ' < "$JVM_OPTIONS_FILE")
-  else
-    echo "jvm.config is not found, using default jvm configurations"
-    PIXELS_JVM_OPTS="-Xmx1024M -server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
-  fi
-fi
-
-echo "JVM options: ${PIXELS_JVM_OPTS}"
-
 # Parse commands
 while [ $# -gt 0 ]; do
   COMMAND=$1
@@ -75,6 +62,19 @@ while [ $# -gt 0 ]; do
       ;;
   esac
 done
+
+# Set JVM options file path or use default JVM options
+JVM_OPTIONS_FILE="$PIXELS_HOME/$DAEMON_ROLE-bin/jvm.config"
+if [ -z "$PIXELS_JVM_OPTS" ]; then
+  if [ -e "$JVM_OPTIONS_FILE" ]; then
+    PIXELS_JVM_OPTS=$(tr '\n' ' ' < "$JVM_OPTIONS_FILE")
+  else
+    echo "jvm.config is not found, using default jvm configurations"
+    PIXELS_JVM_OPTS="-Xmx1024M -server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
+  fi
+fi
+
+echo "JVM options: ${PIXELS_JVM_OPTS}"
 
 PIXELS_OPTS="-Dio.netty.leakDetection.level=advanced"
 if [ "x$OPERATION" = "x" ]; then
