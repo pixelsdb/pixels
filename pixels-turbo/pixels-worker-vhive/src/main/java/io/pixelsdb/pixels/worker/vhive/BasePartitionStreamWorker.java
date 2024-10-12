dc884bbdc020e09c69e7b0ad946721ed603ebe36
jasha64 <yijunma@student.ethz.ch>
2024-10-12 21:14:45 +0200

    Comments

--- pixels-turbo/pixels-worker-vhive/src/main/java/io/pixelsdb/pixels/worker/vhive/BasePartitionStreamWorker.java
+++ pixels-turbo/pixels-worker-vhive/src/main/java/io/pixelsdb/pixels/worker/vhive/BasePartitionStreamWorker.java
@@ -73,6 +73,8 @@ public class BasePartitionStreamWorker extends Worker<PartitionInput, PartitionO
         this.workerMetrics = context.getWorkerMetrics();
         this.workerCoordinateService = new WorkerCoordinateService("128.110.218.225", 18894);
         // Hardcoded for Cloudlab. todo: Need to figure out how to get the daemon IP dynamically.
+        //  Perhaps add a field in the WorkerContext class to store the daemon IP,
+        //  or to have the Pixels planner pass the daemon IP in the Input.
     }
 
     @Override
