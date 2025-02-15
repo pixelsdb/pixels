4cbc4fa4799181c3b9840837124120bcc82db10a
Bian Haoqiong <bianhaoqiong@gmail.com>
2025-02-16 00:36:37 +0800

    add index rpc definition

--- pixels-common/src/main/java/io/pixelsdb/pixels/common/index/MainIndex.java
+++ pixels-common/src/main/java/io/pixelsdb/pixels/common/index/MainIndex.java
@@ -17,7 +17,7 @@
  * License along with Pixels.  If not, see
  * <https://www.gnu.org/licenses/>.
  */
-package io.pixelsdb.pixels.index.main;
+package io.pixelsdb.pixels.common.index;
 
 /**
  * The main index of a table is the mapping from row id to data file.
