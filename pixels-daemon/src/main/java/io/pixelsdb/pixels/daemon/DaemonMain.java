ee2df0e0ec1af722a93a23759008598b26e7fef4
dongyang geng <gengdy.ruc@gmail.com>
2025-03-09 01:55:19 +0000

    fix/delete error merge

--- pixels-daemon/src/main/java/io/pixelsdb/pixels/daemon/DaemonMain.java
+++ pixels-daemon/src/main/java/io/pixelsdb/pixels/daemon/DaemonMain.java
@@ -131,9 +131,6 @@ public class DaemonMain
                         CacheWorker cacheWorker = new CacheWorker();
                         container.addServer("cache_worker", cacheWorker);
                     }
-                    // start retina server on worker node
-                    RetinaServer retinaServer = new RetinaServer(retinaServerPort);
-                    container.addServer("retina", retinaServer);
                 } catch (Throwable e)
                 {
                     log.error("failed to start worker", e);
