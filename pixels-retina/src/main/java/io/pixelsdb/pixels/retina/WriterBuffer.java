0a86096b8c638fcaf1452cb82fdea93413cc4846
dongyang geng <gengdy.ruc@gmail.com>
2025-03-18 11:02:28 +0000

    feat/add basic writer buffer and reader

---
+++ pixels-retina/src/main/java/io/pixelsdb/pixels/retina/WriterBuffer.java
@@ -0,0 +1,393 @@
+/*
+ * Copyright 2025 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+package io.pixelsdb.pixels.retina;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static io.pixelsdb.pixels.common.utils.Constants.DEFAULT_HDFS_BLOCK_SIZE;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.checkerframework.checker.units.qual.A;
+
+import io.pixelsdb.pixels.common.metadata.MetadataService;
+import io.pixelsdb.pixels.common.physical.Storage;
+import io.pixelsdb.pixels.common.physical.StorageFactory;
+import io.pixelsdb.pixels.common.utils.DateUtil;
+import io.pixelsdb.pixels.core.PixelsWriter;
+import io.pixelsdb.pixels.core.PixelsWriterImpl;
+import io.pixelsdb.pixels.core.TypeDescription;
+import io.pixelsdb.pixels.core.encoding.EncodingLevel;
+import io.pixelsdb.pixels.core.vector.ColumnVector;
+import io.pixelsdb.pixels.core.vector.VectorizedRowBatch;
+
+public class WriterBuffer
+{
+    private static final Logger logger = LogManager.getLogger(WriterBuffer.class);
+
+    // 
+    private final TypeDescription schema;
+    private final String schemaName;
+    private final String tableName;
+    
+    //
+    private final VectorizedRowBatch buffer1;
+    private final VectorizedRowBatch buffer2;
+
+    private final AtomicReference<VectorizedRowBatch> activeBuffer;
+
+    // 
+    private final AtomicBoolean buffer1Active;
+    private final AtomicBoolean buffer1Immutable;
+    private final AtomicBoolean buffer2Immutable;
+
+    private final AtomicInteger buffer1RowCount;
+    private final AtomicInteger buffer2RowCount;
+
+    private final int pixelsStride;
+    private final int rowGroupSize;
+    private final long blockSize;
+    private final short replication;
+    private final EncodingLevel encodingLevel;
+    private final boolean nullsPadding;
+    private final int maxBufferSize;
+
+    private final ExecutorService flushExecutor;
+
+    private WriterBuffer(
+            TypeDescription schema,
+            String schemaName,
+            String tableName,
+            int pixelStride,
+            int rowGroupSize,
+            long blockSize,
+            short replication,
+            EncodingLevel encodingLevel,
+            boolean nullsPadding,
+            int maxBufferSize)
+    {
+        this.schema = schema;
+        this.schemaName = schemaName;
+        this.tableName = tableName;
+        this.pixelsStride = pixelStride;
+        this.rowGroupSize = rowGroupSize;
+        this.blockSize = blockSize;
+        this.replication = replication;
+        this.encodingLevel = encodingLevel;
+        this.nullsPadding = nullsPadding;
+        this.maxBufferSize = maxBufferSize;
+
+        this.buffer1 = schema.createRowBatchWithHiddenColumn(pixelStride, TypeDescription.Mode.NONE);
+        this.buffer2 = schema.createRowBatchWithHiddenColumn(pixelStride, TypeDescription.Mode.NONE);
+        this.activeBuffer = new AtomicReference<>(buffer1);
+        this.buffer1Active = new AtomicBoolean(true);
+        this.buffer1Immutable = new AtomicBoolean(false);
+        this.buffer2Immutable = new AtomicBoolean(false);
+        this.buffer1RowCount = new AtomicInteger(0);
+        this.buffer2RowCount = new AtomicInteger(0);
+
+        this.flushExecutor = Executors.newSingleThreadExecutor(r -> {
+            Thread t = new Thread(r, "WriterBuffer-Flush-" + schemaName + "." + tableName);
+            t.setDaemon(true);
+            return t;
+        });
+    }
+
+    public static class Builder
+    {
+        private TypeDescription builderSchema;
+        private String builderSchemaName;
+        private String builderTableName;
+        private int builderPixelStride = 0;
+        private int builderRowGroupSize = 0;
+        private long builderBlockSize = DEFAULT_HDFS_BLOCK_SIZE;
+        private short builderReplication = 3;
+        private EncodingLevel builderEncodingLevel = EncodingLevel.EL0;
+        private boolean builderNullsPadding = false;
+        private int builderMaxBufferSize;
+        
+        private Builder()
+        {
+        }
+
+        private Builder setSchema(TypeDescription schema)
+        {
+            this.builderSchema = schema;
+            return this;
+        }
+
+        private Builder setSchemaName(String schemaName)
+        {
+            this.builderSchemaName = schemaName;
+            return this;
+        }
+
+        private Builder setTableName(String tableName)
+        {
+            this.builderTableName = tableName;
+            return this;
+        }
+
+        public Builder setPixelStride(int stride)
+        {
+            if (stride % 8 != 0)
+            {
+                logger.warn("Pixel stride is recommended to be multiple of 8 for better performance");
+            }
+            this.builderPixelStride = stride;
+            return this;
+        }
+
+        public Builder setRowGroupSize(int rowGroupSize)
+        {
+            this.builderRowGroupSize = rowGroupSize;
+            return this;
+        }
+
+        public Builder setBlockSize(long blockSize)
+        {
+            checkArgument(blockSize > 0, "block size should be positive");
+            this.builderBlockSize = blockSize;
+            return this;
+        }
+
+        public Builder setReplication(short replication)
+        {
+            checkArgument(replication > 0, "num of replicas should be positive");
+            this.builderReplication = replication;
+            return this;
+        }
+
+        public Builder setNullsPadding(boolean nullsPadding)
+        {
+            this.builderNullsPadding = nullsPadding;
+            return this;
+        }
+
+        public Builder setEncodingLevel(EncodingLevel encodingLevel)
+        {
+            this.builderEncodingLevel = encodingLevel;
+            return this;
+        }
+        
+        public Builder setMaxBufferSize(int maxBufferSize)
+        {
+            this.builderMaxBufferSize = maxBufferSize;
+            return this;
+        }
+
+        public WriterBuffer build() throws Exception
+        {
+            return new WriterBuffer(
+                builderSchema,
+                builderSchemaName,
+                builderTableName,
+                builderPixelStride,
+                builderRowGroupSize,
+                builderBlockSize,
+                builderReplication,
+                builderEncodingLevel,
+                builderNullsPadding,
+                builderMaxBufferSize);
+        };
+    }
+
+    public boolean addRow(String[] values, long timestamp)
+    {
+        checkArgument(values.length == schema.getChildren().size(),
+                "Column values count does not match schema column count");
+
+        VectorizedRowBatch buffer = activeBuffer.get();
+        AtomicInteger rowCount = (buffer == buffer1) ? buffer1RowCount : buffer2RowCount;
+        AtomicBoolean immutable = (buffer == buffer1) ? buffer1Immutable : buffer2Immutable;
+
+        if (immutable.get() || rowCount.get() >= maxBufferSize) {
+            if (!switchBuffer()) {
+                return false;
+            }
+
+            buffer = activeBuffer.get();
+            rowCount = (buffer == buffer1) ? buffer1RowCount : buffer2RowCount;
+            immutable = (buffer == buffer1) ? buffer1Immutable : buffer2Immutable;
+        }
+
+        int rowIndex = rowCount.getAndIncrement();
+
+        if (rowIndex >= maxBufferSize) {
+            rowCount.decrementAndGet();
+            return false;
+        }
+
+        ColumnVector[] columnVectors = buffer.cols;
+        for (int i = 0; i < values.length; ++i) {
+            if (values[i] == null) {
+                columnVectors[i].noNulls = false;
+                columnVectors[i].isNull[rowIndex] = true;
+            } else {
+                columnVectors[i].add(values[i]);
+            }
+        }
+
+        columnVectors[columnVectors.length - 1].isNull[rowIndex] = false;
+        columnVectors[columnVectors.length - 1].add(timestamp);
+
+        buffer.size = Math.max(buffer.size, rowIndex + 1);
+
+        if (rowCount.get() >= maxBufferSize) {
+            switchBuffer();
+        }
+
+        return true;
+    }
+    
+    private boolean switchBuffer()
+    {
+        boolean isBuffer1Active = buffer1Active.get();
+        VectorizedRowBatch currentBuffer = isBuffer1Active ? buffer1 : buffer2;
+        AtomicBoolean currentImmutable = isBuffer1Active ? buffer1Immutable : buffer2Immutable;
+        AtomicInteger currentRowCount = isBuffer1Active ? buffer1RowCount : buffer2RowCount;
+
+        // current buffer is still available
+        if (!currentImmutable.get() && currentRowCount.get() < maxBufferSize) {
+            return true;
+        }
+
+        AtomicBoolean otherImmutable = isBuffer1Active ? buffer2Immutable : buffer1Immutable;
+        if (otherImmutable.get()) {
+            return false; // another buffer is not available
+        }
+
+        currentImmutable.set(true);
+
+        VectorizedRowBatch newBuffer = isBuffer1Active ? buffer2 : buffer1;
+        activeBuffer.set(newBuffer);
+        buffer1Active.set(!isBuffer1Active);
+
+        final VectorizedRowBatch bufferToFlush = currentBuffer;
+        final boolean isBuffer1 = isBuffer1Active;
+
+        flushExecutor.submit(() -> {
+            try 
+            {
+                flushBuffer(bufferToFlush, isBuffer1);
+            } catch (IOException e) 
+            {
+                logger.error("Failed to flush buffer ", e);
+            }
+        });
+
+        return true;
+    }
+    
+    private void flushBuffer(VectorizedRowBatch buffer, boolean isBuffer1) throws IOException
+    {
+        if (buffer.size == 0) {
+            if (isBuffer1) {
+                buffer1Immutable.set(false);
+                buffer1RowCount.set(0);
+            } else {
+                buffer1Immutable.set(false);
+                buffer1RowCount.set(0);
+            }
+            return;
+        }
+
+        try {
+            String targetDirPath = "";
+            String targetFileName = DateUtil.getCurTime() + ".pxl";
+            String targetFilePath = targetDirPath + targetFileName;
+
+            Storage targetStorage = StorageFactory.Instance().getStorage(targetDirPath);
+
+            PixelsWriter pixelsWriter = PixelsWriterImpl.newBuilder()
+                    .setSchema(schema)
+                    .setHasHiddenColumn(true)
+                    .setPixelStride(pixelsStride)
+                    .setRowGroupSize(rowGroupSize)
+                    .setStorage(targetStorage)
+                    .setPath(targetFilePath)
+                    .setBlockSize(blockSize)
+                    .setReplication(replication)
+                    .setBlockPadding(true)
+                    .setEncodingLevel(encodingLevel)
+                    .setNullsPadding(nullsPadding)
+                    .setCompressionBlockSize(1)
+                    .build();
+
+            pixelsWriter.addRowBatch(buffer);
+            pixelsWriter.close();
+
+            buffer.reset();
+            if (isBuffer1) {
+                buffer1Immutable.set(false);
+                buffer1RowCount.set(0);
+            } else {
+                buffer2Immutable.set(false);
+                buffer1RowCount.set(0);
+            }
+        } catch (Exception e) {
+            logger.error("Failed to flush buffer ", e);
+            throw new IOException("Failed to flush buffer ", e);
+        }
+    }
+    
+    public void close() throws IOException
+    {
+        try
+        {
+            if (buffer1RowCount.get() > 0 && !buffer1Immutable.getAndSet(true))
+            {
+                flushBuffer(buffer1, true);
+            }
+    
+            if (buffer2RowCount.get() > 0 && !buffer2Immutable.getAndSet(true))
+            {
+                flushBuffer(buffer2, false);
+            }
+
+            flushExecutor.shutdown();
+            try
+            {
+                if (!flushExecutor.awaitTermination(10, TimeUnit.SECONDS))
+                {
+                    flushExecutor.shutdownNow();
+                }
+            } catch (InterruptedException e)
+            {
+                flushExecutor.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
+        } catch (Exception e)
+        {
+            logger.error("Falied to close WriterBuffer ", e);
+            throw new IOException("Falied to close WriterBuffer ", e);
+        }
+    }
+}
---
+++ pixels-retina/src/main/java/io/pixelsdb/pixels/retina/WriterBufferRecordReader.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2025 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+package io.pixelsdb.pixels.retina;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.pixelsdb.pixels.core.TypeDescription;
+import io.pixelsdb.pixels.core.reader.PixelsRecordReader;
+import io.pixelsdb.pixels.core.vector.VectorizedRowBatch;
+
+public class WriterBufferRecordReader implements PixelsRecordReader
+{
+    private static final Logger logger = LogManager.getLogger(WriterBufferRecordReader.class);
+
+    private final TypeDescription schema;
+    private VectorizedRowBatch rowBatch;
+
+    private final AtomicLong completedRows = new AtomicLong(0);
+    private final AtomicLong completedBytes = new AtomicLong(0);
+    private final AtomicLong readTimeNanos = new AtomicLong(0);
+    private final AtomicLong memoryUsage = new AtomicLong(0);
+
+    private boolean isValid = true;
+    private boolean isEndOfFile = false;
+
+    public WriterBufferRecordReader(TypeDescription schema)
+    {
+        this.schema = schema;
+        this.rowBatch = schema.createRowBatchWithHiddenColumn();
+    }
+
+    @Override
+    public int prepareBatch(int batchSize) throws IOException
+    {
+        return Math.min(batchSize, this.rowBatch.getMaxSize());
+    }
+
+    @Override
+    public VectorizedRowBatch readBatch(int batchSize, boolean reuse) throws IOException
+    {
+        long startTime = System.nanoTime();
+
+        if (!isValid || isEndOfFile)
+        {
+            return createEmptyBatch(reuse);
+        }
+
+        VectorizedRowBatch result;
+        if (reuse && rowBatch != null)
+        {
+            result = rowBatch;
+            result.reset();
+        } else
+        {
+            result = schema.createRowBatchWithHiddenColumn();
+        }
+
+        result.size = 0;
+        completedRows.addAndGet(result.size);
+        
+        long bytesRead = estimateBytesRead(result);
+        completedBytes.addAndGet(bytesRead);
+
+        if (result.size < batchSize)
+        {
+            isEndOfFile = true;
+            result.endOfFile = true;
+        }
+
+        readTimeNanos.addAndGet(System.nanoTime() - startTime);
+        return result;
+    }
+
+    private long estimateBytesRead(VectorizedRowBatch batch)
+    {
+        long bytesPerRow = 0;
+        for (int i = 0; i < schema.getChildren().size(); ++i)
+        {
+            TypeDescription fieldType = schema.getChildren().get(i);
+            switch (fieldType.getCategory()) {
+                case BOOLEAN:
+                case BYTE:
+                    bytesPerRow += 1;
+                    break;
+                case SHORT:
+                    bytesPerRow += 2;
+                    break;
+                case INT:
+                case FLOAT:
+                case DATE:
+                    bytesPerRow += 4;
+                    break;
+                case LONG:
+                case DOUBLE:
+                case TIMESTAMP:
+                    bytesPerRow += 8;
+                    break;
+                case DECIMAL:
+                    bytesPerRow += 16;
+                    break;
+                case STRING:
+                case BINARY:
+                    bytesPerRow += 32;
+                    break;
+                default:
+                    bytesPerRow += 8;
+            }
+        }
+        return bytesPerRow * batch.size;
+    }
+
+    private VectorizedRowBatch createEmptyBatch(boolean reuse)
+    {
+        VectorizedRowBatch result;
+        if (reuse && rowBatch != null)
+        {
+            result = rowBatch;
+            result.reset();
+        } else
+        {
+            result = schema.createRowBatchWithHiddenColumn();
+        }
+        result.size = 0;
+        result.endOfFile = true;
+        return result;
+    }
+
+    @Override
+    public VectorizedRowBatch readBatch(int batchSize) throws IOException
+    {
+        return readBatch(batchSize, false);
+    }
+
+    @Override
+    public VectorizedRowBatch readBatch(boolean reuse) throws IOException
+    {
+        return readBatch(VectorizedRowBatch.DEFAULT_SIZE, reuse);
+    }
+
+    @Override
+    public VectorizedRowBatch readBatch() throws IOException
+    {
+        return readBatch(VectorizedRowBatch.DEFAULT_SIZE, false);
+    }
+
+    @Override
+    public TypeDescription getResultSchema()
+    {
+        return schema;
+    }
+
+    @Override
+    public boolean isValid()
+    {
+        return isValid;
+    }
+
+    @Override
+    public boolean isEndOfFile()
+    {
+        return isEndOfFile;
+    }
+
+    @Override
+    public boolean seekToRow(long rowIndex) throws IOException
+    {
+        throw new UnsupportedOperationException("Seek operation is not supported");
+    }
+
+    @Override
+    public boolean skip(long rowNum) throws IOException
+    {
+        throw new UnsupportedOperationException("Skip operation is not supported");
+    }
+
+    @Override
+    public long getCompletedRows()
+    {
+        return completedRows.get();
+    }
+
+    @Override
+    public long getCompletedBytes()
+    {
+        return completedBytes.get();
+    }
+
+    @Override
+    public int getNumReadRequests()
+    {
+        return 0;
+    }
+
+    @Override
+    public long getReadTimeNanos()
+    {
+        return readTimeNanos.get();
+    }
+
+    @Override
+    public long getMemoryUsage()
+    {
+        return memoryUsage.get();
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        rowBatch = null;
+        isValid = false;
+        isEndOfFile = true;
+        logger.info("Closed WriterBufferRecordReader");
+    }
+}
