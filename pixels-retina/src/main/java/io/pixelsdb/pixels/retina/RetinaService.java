9e03f412a022eeb229669324c3db2c8f62d6a852
Haoqiong Bian <bianhaoqiong@gmail.com>
2025-05-27 17:59:13 +0800

    [Issue #895] use the spike-java sdk from maven central (#896)

--- pixels-retina/src/main/java/io/pixelsdb/pixels/retina/RetinaService.java
+++ pixels-retina/src/main/java/io/pixelsdb/pixels/retina/RetinaService.java
@@ -21,12 +21,10 @@ package io.pixelsdb.pixels.retina;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.pixelsdb.pixels.common.error.ErrorCode;
 import io.pixelsdb.pixels.common.exception.RetinaException;
 import io.pixelsdb.pixels.core.utils.Bitmap;
 import io.pixelsdb.pixels.core.vector.VectorizedRowBatch;
 
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class RetinaService
@@ -50,70 +48,13 @@ public class RetinaService
 
     public VectorizedRowBatch queryRecords(String schemaName, String tableName, int rgid, long timestamp) throws RetinaException
     {
-        String token = UUID.randomUUID().toString();
-        RetinaProto.RequestHeader header = RetinaProto.RequestHeader.newBuilder().setToken(token).build();
-
-        RetinaProto.QueryRecordsRequest request = RetinaProto.QueryRecordsRequest.newBuilder()
-                .setHeader(header)
-                .setSchemaName(schemaName)
-                .setTableName(tableName)
-                .setRgid(rgid)
-                .setTimestamp(timestamp)
-                .build();
-        try
-        {
-            RetinaProto.QueryRecordsResponse response = this.stub.queryRecords(request);
-            if (response.getHeader().getErrorCode() != ErrorCode.SUCCESS)
-            {
-                throw new RetinaException("failed to queryRecords, error code=" + response.getHeader().getErrorCode());
-            }
-            long pos = response.getPos();
-            // TODO: read records from shared memory.
-//            MemoryMappedFile mmf = new MemoryMappedFile();
-
-            RetinaProto.QueryAck ack = RetinaProto.QueryAck.newBuilder()
-                    .setHeader(header)
-                    .setPos(pos)
-                    .build();
-            RetinaProto.ResponseHeader response_ = this.stub.finishRecords(ack);
-            if (response_.getErrorCode() != ErrorCode.SUCCESS)
-            {
-                throw new RetinaException("failed to ack queryRecords, error code=" + response.getHeader().getErrorCode());
-            }
-
-            return new VectorizedRowBatch(0);
-        } catch (Exception e)
-        {
-            throw new RetinaException("failed to queryRecords", e);
-        }
+        // TODO: implement
+        return null;
     }
 
     public Bitmap getVisibility(String schemaName, String tableName, int rgid, long timestamp) throws RetinaException
     {
-        String token = UUID.randomUUID().toString();
-        RetinaProto.RequestHeader header = RetinaProto.RequestHeader.newBuilder().setToken(token).build();
-
-        RetinaProto.QueryVisibilityRequest request = RetinaProto.QueryVisibilityRequest.newBuilder()
-                .setHeader(header)
-                .setSchemaName(schemaName)
-                .setTableName(tableName)
-                .setRgid(rgid)
-                .setTimestamp(timestamp)
-                .build();
-        try
-        {
-            RetinaProto.QueryVisibilityResponse response = this.stub.queryVisibility(request);
-            if (response.getHeader().getErrorCode() != ErrorCode.SUCCESS)
-            {
-                throw new RetinaException("failed to queryRecords, error code=" + response.getHeader().getErrorCode());
-            }
-            long pos = response.getPos();
-            // TODO: read bitmap from shared memory.
-//            MemoryMappedFile mmf = new MemoryMappedFile();
-            return new Bitmap(0, false);
-        } catch (Exception e)
-        {
-            throw new RetinaException("failed to queryRecords", e);
-        }
+        // TODO: implement
+        return null;
     }
 }
