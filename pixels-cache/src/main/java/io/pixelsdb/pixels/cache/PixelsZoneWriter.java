41589606ebd36a07e414b8a55a614529e054670e
Haoqiong Bian <bianhaoqiong@gmail.com>
2025-08-16 01:03:42 +0800

    revise interface

--- pixels-cache/src/main/java/io/pixelsdb/pixels/cache/PixelsZoneWriter.java
+++ pixels-cache/src/main/java/io/pixelsdb/pixels/cache/PixelsZoneWriter.java
@@ -43,8 +43,8 @@ public class PixelsZoneWriter
     private long currentIndexOffset;
     private long allocatedIndexOffset = PixelsZoneUtil.INDEX_RADIX_OFFSET;
     private long cacheOffset = PixelsZoneUtil.ZONE_DATA_OFFSET; // this is only used in the write() method.
-    private ByteBuffer nodeBuffer = ByteBuffer.allocate(8 * 256);
-    private ByteBuffer cacheIdxBuffer = ByteBuffer.allocate(PixelsCacheIdx.SIZE);
+    private final ByteBuffer nodeBuffer = ByteBuffer.allocate(8 * 256);
+    private final ByteBuffer cacheIdxBuffer = ByteBuffer.allocate(PixelsCacheIdx.SIZE);
 
     public PixelsZoneWriter(String builderZoneLocation, String builderIndexLocation,
                             long builderZoneSize, long builderIndexSize, int zoneId) throws IOException
