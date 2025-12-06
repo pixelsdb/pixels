0230a8ea1edd6234f0e93a5a72eca66c796dce6d
Haoqiong Bian <bianhaoqiong@gmail.com>
2025-12-06 23:56:29 +0800

    add test

---
+++ pixels-common/src/test/java/io/pixelsdb/pixels/common/transaction/TestTransService.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 PixelsDB.
+ *
+ * This file is part of Pixels.
+ *
+ * Pixels is free software: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Pixels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Affero GNU General Public License for more details.
+ *
+ * You should have received a copy of the Affero GNU General Public
+ * License along with Pixels.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+package io.pixelsdb.pixels.common.transaction;
+
+import io.pixelsdb.pixels.common.exception.TransException;
+import org.junit.Test;
+
+/**
+ * @author hank
+ * @create 2025-12-06
+ */
+public class TestTransService
+{
+    @Test
+    public void testExtendTransLease() throws TransException
+    {
+        TransService service = TransService.Instance();
+        TransContext context = service.beginTrans(false);
+        service.extendTransLease(context.getTransId());
+    }
+}
