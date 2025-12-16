5d9e3d7abab0786cf515ba1ce66178400f2326da
AntiO2 <antio2@qq.com>
2025-12-16 12:54:12 +0000

    Fix: missing RetinaService resolution when querying tables without primary index

--- pixels-common/src/main/java/io/pixelsdb/pixels/common/utils/Constants.java
+++ pixels-common/src/main/java/io/pixelsdb/pixels/common/utils/Constants.java
@@ -98,7 +98,7 @@ public final class Constants
 
     public static final String CF_OUTPUT_STATE_KEY_PREFIX = "pixels_turbo_cf_output";
 
-    public static final String LOAD_DEFAULT_RETINA_PREFIX = "default_retina_";
+    public static final String LOAD_DEFAULT_RETINA_PREFIX = "default-retina";
 
     public static final int LEASE_TIME_SKEW_MS = 500;
     public static final int LEASE_NETWORK_LATENCY_MS = 500;
